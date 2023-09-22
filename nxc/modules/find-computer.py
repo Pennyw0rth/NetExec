f5439cb43f992bafa43f17dee8818bbfa56a9578
Marshall Hallenbeck <Marshall.Hallenbeck@gmail.com>
2023-09-21 23:12:59 -0400

    clean up find-computer module

--- nxc/modules/find-computer.py
+++ nxc/modules/find-computer.py
@@ -1,84 +1,88 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import socket
+from nxc.logger import nxc_logger
+from impacket.ldap.ldap import LDAPSearchError
+from impacket.ldap.ldapasn1 import SearchResultEntry
+
 
 class NXCModule:
-    '''
+    """
       Module by CyberCelt: @Cyb3rC3lt
 
       Initial module:
         https://github.com/Cyb3rC3lt/CrackMapExec-Modules
-    '''
+    """
     
-    name = 'find-computer'
-    description = 'Finds computers in the domain via the provided text'
-    supported_protocols = ['ldap']
+    name = "find-computer"
+    description = "Finds computers in the domain via the provided text"
+    supported_protocols = ["ldap"]
     opsec_safe = True
     multiple_hosts = False
 
     def options(self, context, module_options):
-        '''
+        """
         find-computer: Specify find-computer to call the module
         TEXT: Specify the TEXT option to enter your text to search for
         Usage: nxc ldap $DC-IP -u Username -p Password -M find-computer -o TEXT="server"
                nxc ldap $DC-IP -u Username -p Password -M find-computer -o TEXT="SQL"
-        '''
+        """
 
-        self.TEXT = ''
+        self.TEXT = ""
 
-        if 'TEXT' in module_options:
-            self.TEXT = module_options['TEXT']
+        if "TEXT" in module_options:
+            self.TEXT = module_options["TEXT"]
         else:
-            context.log.error('TEXT option is required!')
+            context.log.error("TEXT option is required!")
             exit(1)
 
     def on_login(self, context, connection):
-
-        # Building the search filter
-        searchFilter = "(&(objectCategory=computer)(&(|(operatingSystem=*"+self.TEXT+"*)(name=*"+self.TEXT+"*))))"
+        search_filter = f"(&(objectCategory=computer)(&(|(operatingSystem=*{self.TEXT}*))(name=*{self.TEXT}*)))"
 
         try:
-            context.log.debug('Search Filter=%s' % searchFilter)
-            resp = connection.ldapConnection.search(searchFilter=searchFilter,
-                                        attributes=['dNSHostName','operatingSystem'],
-                                        sizeLimit=0)
-        except ldap_impacket.LDAPSearchError as e:
-            if e.getErrorString().find('sizeLimitExceeded') >= 0:
-                context.log.debug('sizeLimitExceeded exception caught, giving up and processing the data received')
+            context.log.debug(f"Search Filter={search_filter}")
+            resp = connection.ldapConnection.search(
+                searchFilter=search_filter,
+                attributes=["dNSHostName", "operatingSystem"],
+                sizeLimit=0
+            )
+        except LDAPSearchError as e:
+            if e.getErrorString().find("sizeLimitExceeded") >= 0:
+                context.log.debug("sizeLimitExceeded exception caught, giving up and processing the data received")
                 resp = e.getAnswers()
                 pass
             else:
-                logging.debug(e)
+                nxc_logger.debug(e)
                 return False
 
         answers = []
-        context.log.debug('Total no. of records returned %d' % len(resp))
+        context.log.debug(f"Total no. of records returned: {len(resp)}")
         for item in resp:
-            if isinstance(item, ldapasn1_impacket.SearchResultEntry) is not True:
+            if isinstance(item, SearchResultEntry) is not True:
                 continue
-            dNSHostName =  ''
-            operatingSystem = ''
+            dns_host_name = ""
+            operating_system = ""
             try:
-                for attribute in item['attributes']:
-                    if str(attribute['type']) == 'dNSHostName':
-                        dNSHostName = str(attribute['vals'][0])
-                    elif str(attribute['type']) == 'operatingSystem':
-                        operatingSystem = attribute['vals'][0]
-                if dNSHostName != '' and operatingSystem != '':
-                    answers.append([dNSHostName,operatingSystem])
+                for attribute in item["attributes"]:
+                    if str(attribute["type"]) == "dNSHostName":
+                        dns_host_name = str(attribute["vals"][0])
+                    elif str(attribute["type"]) == "operatingSystem":
+                        operating_system = attribute["vals"][0]
+                if dns_host_name != "" and operating_system != "":
+                    answers.append([dns_host_name,operating_system])
             except Exception as e:
                 context.log.debug("Exception:", exc_info=True)
-                context.log.debug('Skipping item, cannot process due to error %s' % str(e))
+                context.log.debug(f"Skipping item, cannot process due to error {e}")
                 pass
         if len(answers) > 0:
-            context.log.success('Found the following computers: ')
+            context.log.success("Found the following computers: ")
             for answer in answers:
                 try:
-                    IP = socket.gethostbyname(answer[0])
-                    context.log.highlight(u'{} ({}) ({})'.format(answer[0],answer[1],IP))
-                    context.log.debug('IP found')
+                    ip = socket.gethostbyname(answer[0])
+                    context.log.highlight(f'{answer[0]} ({answer[1]}) ({ip})')
+                    context.log.debug("IP found")
                 except socket.gaierror:
                     context.log.debug('Missing IP')
-                    context.log.highlight(u'{} ({}) ({})'.format(answer[0],answer[1],"No IP Found"))
+                    context.log.highlight(f'{answer[0]} ({answer[1]}) (No IP Found)')
         else:
-            context.log.success('Unable to find any computers with the text "' + self.TEXT + '"')
+            context.log.success(f"Unable to find any computers with the text {self.TEXT}")
