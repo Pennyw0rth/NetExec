69514ce3562b5effbb874778ab605a5c552c05d1
Alex <61382599+NeffIsBack@users.noreply.github.com>
2023-09-23 12:56:04 +0200

    Remove old NotImplementedError

--- nxc/modules/veeam_dump.py
+++ nxc/modules/veeam_dump.py
@@ -102,9 +102,6 @@ class NXCModule:
             except Exception as e:
                 context.log.fail(f"UNEXPECTED ERROR: {e}")
                 context.log.debug(traceback.format_exc())
-
-        except NotImplementedError:
-            pass
         except Exception as e:
             context.log.fail(f"UNEXPECTED ERROR: {e}")
             context.log.debug(traceback.format_exc())
