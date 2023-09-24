9cbd847fd6711f3176cf7a498efc50f08e62c7b6
romanrii <roman.rivas@romanrii.com>
2023-09-24 12:36:14 +0000

    Modified nxc/protocols/ftp/proto_args.py and added (--get and --put). Modified --ls Modified nxc/protocols/ftp.py and added --get and --put functionality. Modified --ls functionality

--- nxc/protocols/ftp/proto_args.py
+++ nxc/protocols/ftp/proto_args.py
@@ -3,5 +3,7 @@ def proto_args(parser, std_parser, module_parser):
     ftp_parser.add_argument("--port", type=int, default=21, help="FTP port (default: 21)")
 
     cgroup = ftp_parser.add_argument_group("FTP Access", "Options for enumerating your access")
-    cgroup.add_argument("--ls", action="store_true", help="List files in the directory")
+    cgroup.add_argument("--ls", metavar="DIRECTORY", nargs="?", const=".", help="List files in the directory")
+    cgroup.add_argument("--get", metavar="FILE", help="Download a file")
+    cgroup.add_argument("--put", metavar=("LOCAL_FILE", "REMOTE_FILE"), nargs=2, help="Upload a file")
     return parser
