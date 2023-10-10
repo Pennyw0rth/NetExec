50f2e5d1c159c88fe1f9d1f67ded1f14170e4827
sandw1ch <se.brink15@gmail.com>
2023-10-10 17:23:12 -0500

    sed "s/'/\"/g"
    Signed-off-by: sandw1ch <se.brink15@gmail.com>

--- nxc/modules/pso.py
+++ nxc/modules/pso.py
@@ -36,72 +36,72 @@ class NXCModule:
 
         # Are there even any FGPPs?
         context.log.success("Attempting to enumerate policies...")
-        resp = connection.ldapConnection.search(searchBase='CN=Password Settings Container,CN=System,'+ base_creator(self.__domain), searchFilter='(objectclass=*)')
+        resp = connection.ldapConnection.search(searchBase="CN=Password Settings Container,CN=System,"+ base_creator(self.__domain), searchFilter="(objectclass=*)")
         if len(resp) > 1:
             context.log.highlight(str(len(resp) - 1) + " PSO Objects found!")
             context.log.highlight("")
             context.log.success("Attempting to enumerate objects with an applied policy...")
 
         # Who do they apply to?
-        resp=connection.ldapConnection.search(searchBase=base_creator(self.__domain), searchFilter='(objectclass=*)',attributes=['DistinguishedName','msDS-PSOApplied'])
+        resp=connection.ldapConnection.search(searchBase=base_creator(self.__domain), searchFilter="(objectclass=*)",attributes=["DistinguishedName","msDS-PSOApplied"])
         for i in resp:
             if isinstance(i, ldapasn1_impacket.SearchResultEntry) is not True:
                 continue
-            for attr in i['attributes']:
-                if (str(attr['type']) == 'msDS-PSOApplied'):
-                    context.log.highlight("Object: " + str(i['objectName']))
-                    if len(attr['vals']) == 1:
+            for attr in i["attributes"]:
+                if (str(attr["type"]) == "msDS-PSOApplied"):
+                    context.log.highlight("Object: " + str(i["objectName"]))
+                    if len(attr["vals"]) == 1:
                         context.log.highlight("Applied Policy: ")
-                        context.log.highlight("\t"+attr['vals'][0])
+                        context.log.highlight("\t"+attr["vals"][0])
                         context.log.highlight("")
                     else:
-                        policies=''
-                        for value in attr['vals']:
+                        policies=""
+                        for value in attr["vals"]:
                             policies=policies+value+";"
                         context.log.highlight("Applied Policy: ")
                         for object in str(policies)[:-1].split(";"):
                             context.log.highlight("\t"+str(object))
                         context.log.highlight("")
 
-        # Let's find out even more details!
+        # Let"s find out even more details!
         context.log.success("Attempting to enumerate details...\n")
-        resp=connection.ldapConnection.search(searchBase=base_creator(self.__domain), searchFilter='(objectclass=msDS-PasswordSettings)',
-                     attributes=['name', 'msds-lockoutthreshold', 'msds-psoappliesto', 'msds-minimumpasswordlength',
-                                 'msds-passwordhistorylength', 'msds-lockoutobservationwindow', 'msds-lockoutduration',
-                                 'msds-passwordsettingsprecedence', 'msds-passwordcomplexityenabled', 'Description',
-                                 'msds-passwordreversibleencryptionenabled','msds-minimumpasswordage','msds-maximumpasswordage'])
+        resp=connection.ldapConnection.search(searchBase=base_creator(self.__domain), searchFilter="(objectclass=msDS-PasswordSettings)",
+                     attributes=["name", "msds-lockoutthreshold", "msds-psoappliesto", "msds-minimumpasswordlength",
+                                 "msds-passwordhistorylength", "msds-lockoutobservationwindow", "msds-lockoutduration",
+                                 "msds-passwordsettingsprecedence", "msds-passwordcomplexityenabled", "Description",
+                                 "msds-passwordreversibleencryptionenabled","msds-minimumpasswordage","msds-maximumpasswordage"])
         for i in resp:
             if isinstance(i, ldapasn1_impacket.SearchResultEntry) is not True:
                 continue
             policyName,description,passwordLength,passwordhistorylength,lockoutThreshold,obersationWindow,lockoutDuration,complexity,minPassAge,maxPassAge,reverseibleEncryption,precedence,policyApplies=("",)*13
-            for attr in i['attributes']:
-                if (str(attr['type']) == 'name'):
-                    name = attr['vals'][0]
-                elif (str(attr['type']) == 'msDS-LockoutThreshold'):
-                    lockoutThreshold = attr['vals'][0]
-                elif (str(attr['type']) == 'msDS-MinimumPasswordLength'):
-                    passwordLength = attr['vals'][0]
-                elif (str(attr['type']) == 'msDS-PasswordHistoryLength'):
-                    passwordhistorylength = attr['vals'][0]
-                elif (str(attr['type']) == 'msDS-LockoutObservationWindow'):
-                    obersationWindow = attr['vals'][0]
-                elif (str(attr['type']) == 'msDS-LockoutDuration'):
-                    lockoutDuration = attr['vals'][0]
-                elif (str(attr['type']) == 'msDS-PasswordSettingsPrecedence'):
-                    precedence = attr['vals'][0]
-                elif (str(attr['type']) == 'msDS-PasswordComplexityEnabled'):
-                    complexity = attr['vals'][0]
-                elif (str(attr['type']) == 'msDS-PasswordReversibleEncryptionEnabled'):
-                    reverseibleEncryption= attr['vals'][0]
-                elif (str(attr['type']) == 'msDS-MinimumPasswordAge'):
-                    minPassAge = attr['vals'][0]
-                elif (str(attr['type']) == 'msDS-MaximumPasswordAge'):
-                    maxPassAge = attr['vals'][0]
-                elif (str(attr['type']) == 'description'):
-                    description = attr['vals'][0]
-                elif (str(attr['type'])) == 'msDS-PSOAppliesTo':
-                    policyApplies = ''
-                    for value in attr['vals']:
+            for attr in i["attributes"]:
+                if (str(attr["type"]) == "name"):
+                    name = attr["vals"][0]
+                elif (str(attr["type"]) == "msDS-LockoutThreshold"):
+                    lockoutThreshold = attr["vals"][0]
+                elif (str(attr["type"]) == "msDS-MinimumPasswordLength"):
+                    passwordLength = attr["vals"][0]
+                elif (str(attr["type"]) == "msDS-PasswordHistoryLength"):
+                    passwordhistorylength = attr["vals"][0]
+                elif (str(attr["type"]) == "msDS-LockoutObservationWindow"):
+                    obersationWindow = attr["vals"][0]
+                elif (str(attr["type"]) == "msDS-LockoutDuration"):
+                    lockoutDuration = attr["vals"][0]
+                elif (str(attr["type"]) == "msDS-PasswordSettingsPrecedence"):
+                    precedence = attr["vals"][0]
+                elif (str(attr["type"]) == "msDS-PasswordComplexityEnabled"):
+                    complexity = attr["vals"][0]
+                elif (str(attr["type"]) == "msDS-PasswordReversibleEncryptionEnabled"):
+                    reverseibleEncryption= attr["vals"][0]
+                elif (str(attr["type"]) == "msDS-MinimumPasswordAge"):
+                    minPassAge = attr["vals"][0]
+                elif (str(attr["type"]) == "msDS-MaximumPasswordAge"):
+                    maxPassAge = attr["vals"][0]
+                elif (str(attr["type"]) == "description"):
+                    description = attr["vals"][0]
+                elif (str(attr["type"])) == "msDS-PSOAppliesTo":
+                    policyApplies = ""
+                    for value in attr["vals"]:
                         policyApplies=policyApplies+value+";"
             context.log.highlight("Policy Name: " + str(name))
             if description != "":
@@ -129,6 +129,6 @@ def base_creator(domain):
     return search_base[:-1]
 
 def clock(nano):
-    fmt = '{0.days} days {0.hours} hours {0.minutes} minutes {0.seconds} seconds'
+    fmt = "{0.days} days {0.hours} hours {0.minutes} minutes {0.seconds} seconds"
     sec = int(abs(nano/10000000))
     return fmt.format(rd(seconds=sec))
