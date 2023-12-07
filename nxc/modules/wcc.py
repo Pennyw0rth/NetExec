Pennyw0rth/NetExec

commit bfd32f19103c4b069f93cb07c7f273d911be40bc

ruff: manually fix flake8-bugbear issues

nxc/modules/wcc.py
@@ -565,7 +565,7 @@ class HostChecker:
             _, _, data = get_value(subkey_handle)
         else:
             found = False
-            for _, name, data in subkey_values(subkey_handle):
+            for _, name, _data in subkey_values(subkey_handle):
                 if name.upper() == valueName.upper():
                     found = True
                     break

nxc/modules/add_computer.py
@@ -156,9 +156,7 @@ class NXCModule:
                 if e.error_code == 0xC0000073:
                     context.log.highlight(f"{self.__computerName} not found in domain {selected_domain}")
                     self.noLDAPRequired = True
-                    raise Exception()
-                else:
-                    raise
+                context.log.exception(e)
 
             user_rid = check_for_user["RelativeIds"]["Element"][0]
             if self.__delete:
@@ -174,9 +172,7 @@ class NXCModule:
                 if e.error_code == 0xC0000022:
                     context.log.highlight(f"{self.__username + ' does not have the right to ' + message + ' ' + self.__computerName}")
                     self.noLDAPRequired = True
-                    raise Exception()
-                else:
-                    raise
+                context.log.exception(e)
         else:
             if self.__computerName is not None:
                 try:
@@ -211,12 +207,9 @@ class NXCModule:
             except samr.DCERPCSessionError as e:
                 if e.error_code == 0xC0000022:
                     context.log.highlight("{}".format('The following user does not have the right to create a computer account: "' + self.__username + '"'))
-                    raise Exception()
                 elif e.error_code == 0xC00002E7:
                     context.log.highlight("{}".format('The following user exceeded their machine account quota: "' + self.__username + '"'))
-                    raise Exception()
-                else:
-                    raise
+                context.log.exception(e)
             user_handle = create_user["UserHandle"]
 
         if self.__delete:

nxc/modules/winscp_dump.py
@@ -101,7 +101,7 @@ class NXCModule:
 
         # decrypt the password
         clearpass = ""
-        for i in range(pw_length):
+        for _i in range(pw_length):
             val, pass_bytes = self.dec_next_char(pass_bytes)
             clearpass += chr(val)
         if pw_flag == self.PW_FLAG:

nxc/protocols/ldap.py
@@ -847,8 +847,6 @@ class ldap(connection):
                         self.logger.highlight(f"{name} = {colored(ip_address, host_info_colors[0])}")
                 except socket.gaierror:
                     self.logger.fail(f"{name} = Connection timeout")
-                except socket.gaierror:
-                    self.logger.fail(f"{name} = Connection timeout")
             except Exception as e:
                 self.logger.fail("Exception:", exc_info=True)
                 self.logger.fail(f"Skipping item, cannot process due to error {e}")
@@ -1004,7 +1002,7 @@ class ldap(connection):
                 self.logger.display(f"Total of records returned {len(answers):d}")
                 TGT = KerberosAttacks(self).get_tgt_kerberoasting()
                 dejavue = []
-                for (_SPN, sAMAccountName, memberOf, pwdLastSet, lastLogon, delegation,) in answers:
+                for (_SPN, sAMAccountName, memberOf, pwdLastSet, lastLogon, _delegation) in answers:
                     if sAMAccountName not in dejavue:
                         downLevelLogonName = self.targetDomain + "\\" + sAMAccountName
 

nxc/protocols/smb.py
@@ -1111,8 +1111,8 @@ class smb(connection):
                 )
 
                 self.logger.success("Enumerated domain computer(s)")
-                for hosts in hosts:
-                    domain, host_clean = self.domainfromdnshostname(hosts.dnshostname)
+                for host in hosts:
+                    domain, host_clean = self.domainfromdnshostname(host.dnshostname)
                     self.logger.highlight(f"{domain}\\{host_clean:<30}")
                 break
             except Exception as e:

commit 313d7958713eafc65e0bfc5832b6a591615b065d

Fix bug introduced by commit bfd32f19103c4b069f93cb07c7f273d911be40bc

nxc/modules/wcc.py
@@ -591,7 +591,7 @@ class HostChecker:
             _, _, data = get_value(subkey_handle)
         else:
             found = False
-            for _, name, _data in subkey_values(subkey_handle):
+            for _, name, data in subkey_values(subkey_handle):
                 if name.upper() == valueName.upper():
                     found = True
                     break