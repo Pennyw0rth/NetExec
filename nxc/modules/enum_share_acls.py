bec4831cfb77388e97c646d3de3f05111cc20312
Dimitar Ganev <d.ganev@protonmail.ch>
2024-01-11 23:23:44 +0200

    Enumerate the uses through SMB

--- nxc/modules/enum_share_acls.py
+++ nxc/modules/enum_share_acls.py
@@ -1,18 +1,213 @@
 # Module by @syl
 # https://twitter.com/5yrull
+# S-1-5-21-3750225908-897271529-2201778708-1103
+# 'S-1-5-21-3750225908-897271529-2201778708'
+# (Pdb) resp = lsad.hLsarQueryInformationPolicy2(dce, policyHandle, lsad.POLICY_INFORMATION_CLASS.PolicyAccountDomainInformation)
+# (Pdb) domainSid = resp['PolicyInformation']['PolicyAccountDomainInfo']['DomainSid'].formatCanonical()
+# (Pdb) domainSid
+# 'S-1-5-21-3750225908-897271529-2201778708'
+# (Pdb) lsat.hLsarLookupSids(dce, policyHandle, ['S-1-5-21-3750225908-897271529-2201778708-1103'],lsat.LSAP_LOOKUP_LEVEL.LsapLookupWksta)
+# <impacket.dcerpc.v5.lsat.LsarLookupSidsResponse object at 0x7f041cba9d50>
+# (Pdb) resp = lsat.hLsarLookupSids(dce, policyHandle, ['S-1-5-21-3750225908-897271529-2201778708-1103'],lsat.LSAP_LOOKUP_LEVEL.LsapLookupWksta)
+# (Pdb) resp
+# <impacket.dcerpc.v5.lsat.LsarLookupSidsResponse object at 0x7f041cba9de0>
+# (Pdb) resp.__dict__
+# {'_isNDR64': False, 'fields': {'ReferencedDomains': <impacket.dcerpc.v5.lsat.PLSAPR_REFERENCED_DOMAIN_LIST object at 0x7f041cba9ea0>, 'TranslatedNames': <impacket.dcerpc.v5.lsat.LSAPR_TRANSLATED_NAMES object at 0x7f041cba9450>, 'MappedCount': <impacket.dcerpc.v5.ndr.NDRULONG object at 0x7f041cba9270>, 'ErrorCode': <impacket.dcerpc.v5.ndr.NDRULONG object at 0x7f041cba8460>}}
+# (Pdb) resp.TranslatedNames
+# *** AttributeError: 'LsarLookupSidsResponse' object has no attribute 'TranslatedNames'
+# (Pdb) resp["TranslatedNames"]
+# <impacket.dcerpc.v5.lsat.LSAPR_TRANSLATED_NAMES object at 0x7f041cba9450>
+# (Pdb) resp["TranslatedNames"].__dict__
+# {'_isNDR64': False, 'fields': {'Entries': <impacket.dcerpc.v5.ndr.NDRULONG object at 0x7f041cba9960>, 'Names': <impacket.dcerpc.v5.lsat.PLSAPR_TRANSLATED_NAME_ARRAY object at 0x7f041cba9a80>}}
+# (Pdb) resp["TranslatedNames"]["Names"]
+# [<impacket.dcerpc.v5.lsat.LSAPR_TRANSLATED_NAME object at 0x7f041cba8a60>]
+# (Pdb) resp["TranslatedNames"]["Names"][0].__dict_
+# *** AttributeError: 'LSAPR_TRANSLATED_NAME' object has no attribute '__dict_'
+# (Pdb) resp["TranslatedNames"]["Names"][0].__dict__
+# {'_isNDR64': False, 'fields': {'Use': <impacket.dcerpc.v5.samr.SID_NAME_USE object at 0x7f041cba8c70>, 'Name': <impacket.dcerpc.v5.dtypes.RPC_UNICODE_STRING object at 0x7f041cba8be0>, 'DomainIndex': <impacket.dcerpc.v5.ndr.NDRLONG object at 0x7f041cba8ca0>}}
+# (Pdb) resp["TranslatedNames"]["Names"][0]["name"]
+# *** KeyError: 'name'
+# (Pdb) resp["TranslatedNames"]["Names"][0]["Name"]
+# 'George.Hotz'
+# (Pdb) 
 
 from impacket.smbconnection import SessionError
+from impacket.dcerpc.v5 import transport, lsat, lsad
+from impacket.dcerpc.v5.samr import SID_NAME_USE
+from impacket.dcerpc.v5.dtypes import MAXIMUM_ALLOWED
+from impacket.dcerpc.v5.rpcrt import DCERPCException
 from winacl.dtyp.security_descriptor import SECURITY_DESCRIPTOR
 from winacl.functions.constants import SE_OBJECT_TYPE
 from winacl.dtyp.ace import FILE_ACCESS_MASK
+from termcolor import colored
 
+def get_dce_from_smb(smb_connection):
+    remote_name = smb_connection._remoteName
+    string_binding = rf"ncacn_np:{remote_name}[\pipe\lsarpc]"
+    rpctransport = transport.DCERPCTransportFactory(string_binding)
+    rpctransport.set_dport(smb_connection._sess_port)
+    rpctransport.setRemoteHost(smb_connection._remoteHost)
+    rpctransport.set_credentials(
+        smb_connection._SMBConnection._SMB__userName,
+        smb_connection._SMBConnection._SMB__password,
+        smb_connection._SMBConnection._SMB__domain,
+        smb_connection._SMBConnection._SMB__lmhash,
+        smb_connection._SMBConnection._SMB__nthash
+    )
+    return rpctransport.get_dce_rpc()
+
+def lookup_sid(dce, sid):
+    dce.connect()
+    dce.bind(lsat.MSRPC_UUID_LSAT)
+    policy_response = lsad.hLsarOpenPolicy2(dce, MAXIMUM_ALLOWED | lsat.POLICY_LOOKUP_NAMES)
+    policy_handle = policy_response["PolicyHandle"]
+    sid_response = lsat.hLsarLookupSids(dce, policy_handle, [sid],lsat.LSAP_LOOKUP_LEVEL.LsapLookupWksta)
+    sid_name = sid_response["TranslatedNames"]["Names"][0]["Name"]
+    dce.disconnect()
+    return sid_name
 
 AVAILABLE_FILTERS = ["ace_mask", "ace_sid", "owner", "group"]
+READ_CONTROL = 0x00020000
+FILE_SHARE_READ = 0x00000001
+FILE_NON_DIRECTORY_FILE = 0x00000040
+FILE_DIRECTORY_FILE = 0x00000001
+IMPERSONATION = 0x00000002
 
+class SMBACLEnumerator:
+    
+    AVAILABLE_FILTERS = ["ace_mask", "ace_sid", "owner", "group"]
+    
+    def __init__(self, connection, context, module_options) -> None:
+        self.connection = connection
+        self.smb_connection = connection.conn
+        self.context = context
+        self.module_options = module_options
+        self.filters = self.parse_filters(self.module_options)
+        self.shares = self.get_shares()
+        
+    def get_shares(self):
+        shares = self.connection.shares()
+    
+    def parse_filters(self, options: dict) -> dict:
+        ...
+        
+    def get_security_descriptor() -> None:
+        ...
+
+# def get_security_descriptor(file, smb_connection, context, share_name, file_path, filters=None):
+#     """
+#     Retrieve the security descriptor of a file on a share.
+
+#     Parameters
+#     ----------
+#         file: File object.
+#         smb_connection: SMB connection instance.
+#         context: Context for logging.
+#         share_name: Name of the SMB share.
+#         file_path: Path of the file.
+#         filters: Optional filters for security descriptor components.
+
+#     Returns
+#     -------
+#         None: If the file does not meet the filter criteria.
+#     """
+#     file_full_path = file_path + file.get_longname()
+#     tree_id = smb_connection.connectTree(share_name)
+#     file_id = create_file(smb_connection, tree_id, file_full_path)
+
+#     try:
+#         security_descriptor = smb_connection._SMBConnection.query_sec_info(tree_id, file_id)
+#         security_descriptor_obj = parse_security_descriptor(security_descriptor)
+
+#         if filters and not passes_filters(security_descriptor_obj, filters):
+#             return
+
+#         log_security_descriptor(context, file_full_path, security_descriptor_obj)
+#     except SessionError as e:
+#         context.log.debug(e)
+
+
+# def create_file(smb_connection, tree_id, file_full_path):
+#     """Create a file in the SMB share and return its ID."""
+#     return smb_connection.createFile(
+#         tree_id,
+#         file_full_path,
+#         desiredAccess=READ_CONTROL,
+#         shareMode=FILE_SHARE_READ,
+#         creationOption=FILE_NON_DIRECTORY_FILE,
+#         creationDisposition=FILE_DIRECTORY_FILE,
+#         fileAttributes=0,
+#         impersonationLevel=IMPERSONATION,
+#     )
+
+
+# def parse_security_descriptor(security_descriptor):
+#     """Parse the security descriptor from bytes to an object."""
+#     return SECURITY_DESCRIPTOR.from_bytes(
+#         security_descriptor, object_type=SE_OBJECT_TYPE.SE_FILE_OBJECT
+#     )
+
+
+# def passes_filters(security_descriptor_obj, filters):
+#     """
+#     Check if the security descriptor object passes the given filter criteria.
+
+#     Parameters
+#     ----------
+#         security_descriptor_obj: The security descriptor object.
+#         filters: Dictionary of filter criteria.
+
+#     Returns
+#     -------
+#         bool: True if the descriptor passes all filters, False otherwise.
+#     """
+#     for filter_type, values in filters.items():
+#         if filter_type == "owner" and str(security_descriptor_obj.Owner) not in values:
+#             return False
+#         if filter_type == "group" and str(security_descriptor_obj.Group) not in values:
+#             return False
+#         if filter_type == "ace_sid":
+#             sids = [str(ace.Sid) for ace in security_descriptor_obj.Dacl.aces]
+#             if not any(sid in sids for sid in values):
+#                 return False
+#         if filter_type == "ace_mask":
+#             masks = [FILE_ACCESS_MASK(ace.Mask).name for ace in security_descriptor_obj.Dacl.aces]
+#             if not any(mask in masks for mask in values):
+#                 return False
+#     return True
+
+
+def log_security_descriptor(context, file_path, descriptor_obj):
+    """
+    Log details about the security descriptor.
+
+    Parameters
+    ----------
+        context: Context for logging.
+        file_path: Path of the file.
+        descriptor_obj: The security descriptor object.
+    """
+    context.log.success(file_path)
+    context.log.highlight(f"\t Owner: {descriptor_obj.Owner}")
+    context.log.highlight(f"\t Group: {descriptor_obj.Group}")
+    context.log.highlight(f"\t SACL: {descriptor_obj.Sacl}")
+
+    if descriptor_obj.Dacl is not None:
+        context.log.highlight(f"\t ACEs[{len(descriptor_obj.Dacl.aces)}]: ")
+        for ace in descriptor_obj.Dacl.aces:
+            ace_str = str(ace)
+            if "\r\n" in ace_str:
+                ace_formatted = ace_str.split("\r\n")[:-1]
+                for line in ace_formatted:
+                    context.log.highlight(f"\t\t {line}")
+                context.log.highlight("\t\t " + len(ace_formatted[0]) * "=")
+            else:
+                context.log.highlight(f"\t\t {ace_str}")
 
 def get_security_descriptor(file, smb_connection, context, share_name, file_path, filters=None):
     """Retrieving the security descriptor of a certain file on a share"""
     tree_id = smb_connection.connectTree(share_name)
+    dce = get_dce_from_smb(smb_connection)
     desired_access = 0x00020000 # READ_CONTROL
     share_mode = 0x00000001 # FILE_SHARE_READ
     create_options = 0x00000040 # FILE_NON_DIRECTORY_FILE
@@ -36,50 +231,49 @@ def get_security_descriptor(file, smb_connection, context, share_name, file_path
             security_descriptor, object_type=SE_OBJECT_TYPE.SE_FILE_OBJECT
         )
 
-        # Instead printing the ACLs, print the human readable thing like,
-        # SID 4959245 has read access to the file policy.xt
-        # SID 4839959 has read/write access to the file policy.txt
-        # SID 4839959 has full access to the file policy.txt
-        
-        # Filters aren't working as of now
         if filters:
             for _filter, values in filters.items():
-                if _filter == "owner":
-                    if security_descriptor_obj.Owner not in values:
-                        return
-                elif _filter == "group":
-                    if security_descriptor_obj.Group not in values:
-                        return
-                elif _filter == "ace_mask":
-                    ace_mask_matched = False
-                    for _ace in security_descriptor_obj.Dacl.aces:
-                        for value in values:
-                            mask = getattr(FILE_ACCESS_MASK, value)
-                            if mask == _ace.Mask:
-                                ace_mask_matched = True
-                                break
-                        if ace_mask_matched:
-                            break
-                    if not ace_mask_matched:
-                        return
-                elif _filter == "ace_sid":
-                    for _ace in security_descriptor_obj.Dacl.aces:
-                        if str(_ace.Sid) not in values:
+                if _filter == "owner" and str(security_descriptor_obj.Owner) not in values:
+                    return
+                if _filter == "group" and str(security_descriptor_obj.Group) not in values:
+                    return
+                if _filter == "ace_sid":
+                    sids = [str(_ace.Sid) for _ace in security_descriptor_obj.Dacl.aces]
+                    for sid in values:
+                        if sid in sids:
+                            continue
+                        else:
+                            return
+                if _filter == "ace_mask":
+                    masks = [FILE_ACCESS_MASK(_ace.Mask).name for _ace in security_descriptor_obj.Dacl.aces]
+                    for mask in values:
+                        if mask in masks:
+                            continue
+                        else:
                             return
 
-        context.log.success(f"Obtained security descriptor for: {file_full_path}")
-        context.log.highlight("\t Owner:", security_descriptor_obj.Owner)
-        context.log.highlight("\t Group:", security_descriptor_obj.Group)
-        context.log.highlight("\t SACL:", security_descriptor_obj.Sacl)
+        context.log.success(file_full_path)
+        # Also get the user's group to mark it as Owned
+        # smb_connection._SMBConnection._SMB__userName
+        username = smb_connection._SMBConnection._SMB__userName
+        
+        owner = lookup_sid(dce, str(security_descriptor_obj.Owner))
+        if owner == username:
+            owner = owner + colored("Own3d!", "red")
+        group = lookup_sid(dce, str(security_descriptor_obj.Group))
+        context.log.highlight(f"\t Owner: {owner}")
+        context.log.highlight(f"\t Group: {group}")
+        context.log.highlight(f"\t SACL: {security_descriptor_obj.Sacl}")
         
         if security_descriptor_obj.Dacl is not None:
-            context.log.highlight(f"\t ACEs[{len(security_descriptor_obj.Dacl.aces)}]: ")
+            context.log.highlight("\t ACEs:")
             for ace in security_descriptor_obj.Dacl.aces:
-                if "\r\n" in str(ace):
-                    ace_formatted = str(ace).split("\r\n")[:-1]
-                    [context.log.highlight(f"\t\t {i}") for i in ace_formatted]
-                    context.log.highlight("\t\t " + len(ace_formatted[0]) * "=")
-                    
+                ace_sid_name = lookup_sid(dce, str(ace.Sid))
+                mask_formatted = FILE_ACCESS_MASK(ace.Mask).name
+                if mask_formatted is None:
+                    continue
+                owned = colored(" (Own3d!)", "red") if ace_sid_name == username else ""
+                context.log.highlight(f"\t\t {ace_sid_name} - {mask_formatted}{owned}")
     except SessionError as e:
         context.log.debug(e)
 
