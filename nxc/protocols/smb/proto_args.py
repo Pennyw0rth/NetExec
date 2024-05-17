66bdcdf1abcefe39dde018e2c65fa32c715945a7
Marshall Hallenbeck <marshall.hallenbeck@gmail.com>
2024-05-16 20:16:44 -0400

    fix command exec method group naming

--- nxc/protocols/smb/proto_args.py
+++ nxc/protocols/smb/proto_args.py
@@ -72,10 +72,10 @@ def proto_args(parser, parents):
     cmd_exec_group.add_argument("--get-output-tries", help="Number of times atexec/smbexec/mmcexec tries to get results", type=int, default=5)
     cmd_exec_group.add_argument("--codec", default="utf-8", help="Set encoding used (codec) from the target's output. If errors are detected, run chcp.com at the target & map the result with https://docs.python.org/3/library/codecs.html#standard-encodings and then execute again with --codec and the corresponding codec")
     cmd_exec_group.add_argument("--no-output", action="store_true", help="do not retrieve command output")
-    # command execution method
-    cemgroup = cmd_exec_group.add_mutually_exclusive_group()
-    cemgroup.add_argument("-x", metavar="COMMAND", dest="execute", help="execute the specified CMD command")
-    cemgroup.add_argument("-X", metavar="PS_COMMAND", dest="ps_execute", help="execute the specified PowerShell command")
+
+    cmd_exec_method_group = cmd_exec_group.add_mutually_exclusive_group()
+    cmd_exec_method_group.add_argument("-x", metavar="COMMAND", dest="execute", help="execute the specified CMD command")
+    cmd_exec_method_group.add_argument("-X", metavar="PS_COMMAND", dest="ps_execute", help="execute the specified PowerShell command")
     
     posh_group = smb_parser.add_argument_group("Powershell Obfuscation", "Options for PowerShell script obfuscation")
     posh_group.add_argument("--obfs", action="store_true", help="Obfuscate PowerShell scripts")
