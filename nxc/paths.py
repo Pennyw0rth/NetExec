9879edffca9708e59a71feac2086601c9318da6d
Marshall Hallenbeck <Marshall.Hallenbeck@gmail.com>
2024-05-23 11:07:05 -0400

    fix(paths): change TMP creation to if/else, so Windows properly sets

--- nxc/paths.py
+++ nxc/paths.py
@@ -3,11 +3,12 @@ import sys
 import nxc
 
 NXC_PATH = os.path.expanduser("~/.nxc")
-TMP_PATH = os.path.join("/tmp", "nxc_hosted")
 if os.name == "nt":
     TMP_PATH = os.getenv("LOCALAPPDATA") + "\\Temp\\nxc_hosted"
-if hasattr(sys, "getandroidapilevel"):
+elif hasattr(sys, "getandroidapilevel"):
     TMP_PATH = os.path.join("/data", "data", "com.termux", "files", "usr", "tmp", "nxc_hosted")
+else:
+    TMP_PATH = os.path.join("/tmp", "nxc_hosted")
 
 CERT_PATH = os.path.join(NXC_PATH, "nxc.pem")
 CONFIG_PATH = os.path.join(NXC_PATH, "nxc.conf")
