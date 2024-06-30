aab22cf4bc1609f8509e5df0021e137dd5c194a0
mpgn <5891788+mpgn@users.noreply.github.com>
2024-06-30 17:33:55 -0400

    update require_admin

--- nxc/connection.py
+++ nxc/connection.py
@@ -84,7 +84,9 @@ def get_host_addr_info(target, force_ipv6, dns_server, dns_tcp, dns_timeout):
 
 def requires_admin(func):
     def _decorator(self, *args, **kwargs):
-        if self.admin_privs is False and self.args.exec_method != "mmcexec":
+        if self.admin_privs is False:
+            if hasattr(self.args, 'exec_method') and self.args.exec_method == "mmcexec":
+                return func(self, *args, **kwargs)
             return None
         return func(self, *args, **kwargs)
 
