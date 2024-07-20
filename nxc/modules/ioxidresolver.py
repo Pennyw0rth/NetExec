6a735c16194d40c6d4888029154fcab4c0ec56f6
NK <nicolas.devillers@airbus.com>
2024-07-20 22:43:15 +0200

    add an option to ioxidresolver to get only interfaces IP for IP different than targets

--- nxc/modules/ioxidresolver.py
+++ nxc/modules/ioxidresolver.py
@@ -17,8 +17,9 @@ class NXCModule:
     multiple_hosts = True
 
     def options(self, context, module_options):
-        """No module options"""
-
+        """DIFFERENT show only ip address if different from target ip (Default: False)"""
+        if module_options and "DIFFERENT" in module_options:
+            self.pivot = module_options.get("DIFFERENT", "false").lower() in ("true", "1")
     def on_login(self, context, connection):
         try:
             rpctransport = transport.DCERPCTransportFactory(f"ncacn_ip_tcp:{connection.host}")
@@ -37,7 +38,11 @@ class NXCModule:
                 NetworkAddr = binding["aNetworkAddr"]
                 try:
                     ip_address(NetworkAddr[:-1])
-                    context.log.highlight(f"Address: {NetworkAddr}")
+                    if self.pivot:
+                        if NetworkAddr.rtrip() != connection.host.rtrip():
+                            context.log.highlight(f"Address: {NetworkAddr}")
+                    else:
+                        context.log.highlight(f"Address: {NetworkAddr}")
                 except Exception as e:
                     context.log.debug(e)
         except DCERPCException as e:
