a140c9479c0d36018950c3808710803a991d066a
Deft_ <aurelien.chalot@protonmail.com>
2024-10-15 19:59:22 +0200

    Minor code optimization
    Signed-off-by: Deft_ <aurelien.chalot@protonmail.com>

--- nxc/modules/recent_files.py
+++ nxc/modules/recent_files.py
@@ -19,21 +19,21 @@ class NXCModule:
     def on_admin_login(self, context, connection):
         lnks = []
         for directory in connection.conn.listPath("C$",  "Users\\*"):
-            if directory.get_longname() not in self.false_positive and directory.is_directory() > 0:
+            if directory.get_longname() not in self.false_positive and directory.is_directory():
                 context.log.highlight(f"C:\\{directory.get_longname()}")
                 recent_files_dir = f"Users\\{directory.get_longname()}\\AppData\\Roaming\\Microsoft\\Windows\\Recent\\"
                 for file in connection.conn.listPath("C$", f"{recent_files_dir}\\*"):
                     file_path = f"{recent_files_dir}{file.get_longname()}"
-                    if file.get_longname() not in self.false_positive:
+                    if file.get_longname() not in self.false_positive and not file.is_directory():
                         file_path = f"{recent_files_dir}{file.get_longname()}"
                         try:
                             buf = BytesIO()
                             connection.conn.getFile("C$", file_path, buf.write)
                             buf.seek(0)
-                            lnk = pylnk3.parse(buf).path
+                            lnk = pylnk3.parse(buf).path.strip()
                             if lnk and lnk not in lnks:
                                 context.log.highlight(f"\t{lnk}")
                                 lnks.append(lnk)
                         except Exception as e:
-                            # needed because of hidden directories in the Recents directory
+                            # Sometimes PyLnk3 can't parse the lnk file...
                             context.log.debug(f"Couldn't open {file_path} because of {e}")
