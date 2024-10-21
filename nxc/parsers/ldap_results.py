a1bb4ee0dfabb49c30985a3b33d95b4783cc8b32
termanix <50464194+termanix@users.noreply.github.com>
2024-10-21 22:05:51 +0300

    update ldap_results for now

--- nxc/parsers/ldap_results.py
+++ nxc/parsers/ldap_results.py
@@ -7,8 +7,13 @@ def parse_result_attributes(ldap_response):
         if not isinstance(entry, ldapasn1_impacket.SearchResultEntry):
             continue
         attribute_map = {}
-        for attribute in entry["attributes"]:
-            val = [str(val).encode(val.encoding).decode("utf-8") for val in attribute["vals"].components]
-            attribute_map[str(attribute["type"])] = val if len(val) > 1 else val[0]
-        parsed_response.append(attribute_map)
+        if not entry["attributes"]:
+            if "objectName" in entry:
+                # Extract the username from the objectName
+                parsed_response.append({"objectName": str(entry["objectName"]), "sAMAccountName": str(entry["objectName"]).split(",")[0].split("=")[1]})
+        else:
+            for attribute in entry["attributes"]:
+                val = [str(val).encode(val.encoding).decode("utf-8") for val in attribute["vals"].components]
+                attribute_map[str(attribute["type"])] = val if len(val) > 1 else val[0]
+            parsed_response.append(attribute_map)
     return parsed_response
