864b7aeed3fb0e3107edfaee7271b93c74243160
mpgn <5891788+mpgn@users.noreply.github.com>
2024-11-03 22:17:57 +0100

    fix proto help

--- nxc/protocols/smb/proto_args.py
+++ nxc/protocols/smb/proto_args.py
@@ -19,7 +19,7 @@ def proto_args(parser, parents):
     smb_parser.add_argument("--gen-relay-list", metavar="OUTPUT_FILE", help="outputs all hosts that don't require SMB signing to the specified file")
     smb_parser.add_argument("--smb-timeout", help="SMB connection timeout", type=int, default=2)
     smb_parser.add_argument("--laps", dest="laps", metavar="LAPS", type=str, help="LAPS authentification", nargs="?", const="administrator")
-    smb_parser.add_argument("--generate-hosts-file", type=str, help="IP for the remote system to connect back to")
+    smb_parser.add_argument("--generate-hosts-file", type=str, help="Generate a hosts file like from a range of IP")
     self_delegate_arg.make_required = [delegate_arg]
 
     cred_gathering_group = smb_parser.add_argument_group("Credential Gathering", "Options for gathering credentials")
