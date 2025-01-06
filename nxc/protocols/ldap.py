0cbd63aa2584d2bfae7ef8f5332322cb142a7ba3
mpgn <5891788+mpgn@users.noreply.github.com>
2025-01-06 09:24:49 +0100

    try to fix test

--- nxc/protocols/ldap.py
+++ nxc/protocols/ldap.py
@@ -205,6 +205,9 @@ class ldap(connection):
                 except Exception as e:
                     self.logger.debug("Exception:", exc_info=True)
                     self.logger.info(f"Skipping item, cannot process due to error {e}")
+        except ConnectionRefusedError as e:
+            self.logger.debug(f"{e} on host {self.host}")
+            return False
         except OSError as e:
             self.logger.error(f"Error getting ldap info {e}")
 
