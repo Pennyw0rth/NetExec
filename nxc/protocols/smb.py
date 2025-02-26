74e6aa03c161ef43a6cee33f2d47aff9621da5aa
Alexander Neff <alex99.neff@gmx.de>
2025-02-26 12:54:51 -0500

    Pass local object to functions istead of using a class variable, alread in use

--- nxc/protocols/smb.py
+++ nxc/protocols/smb.py
@@ -841,11 +841,11 @@ class smb(connection):
             handle = lsm.hRpcOpenEnum()
             rsessions = lsm.hRpcGetEnumResult(handle, Level=1)["ppSessionEnumResult"]
             lsm.hRpcCloseEnum(handle)
-            self.sessions = {}
+            sessions = {}
             for i in rsessions:
                 sess = i["SessionInfo"]["SessionEnum_Level1"]
                 state = TSTS.enum2value(TSTS.WINSTATIONSTATECLASS, sess["State"]).split("_")[-1]
-                self.sessions[sess["SessionId"]] = {
+                sessions[sess["SessionId"]] = {
                     "state": state,
                     "SessionName": sess["Name"],
                     "RemoteIp": "",
@@ -855,24 +855,25 @@ class smb(connection):
                     "Resolution": "",
                     "ClientTimeZone": ""
                 }
+            return sessions
 
-    def enumerate_sessions_info(self):
-        if len(self.sessions):
+    def enumerate_sessions_info(self, sessions):
+        if len(sessions):
             with TSTS.TermSrvSession(self.conn, self.host, self.kerberos) as TermSrvSession:
-                for SessionId in self.sessions:
+                for SessionId in sessions:
                     sessdata = TermSrvSession.hRpcGetSessionInformationEx(SessionId)
                     sessflags = TSTS.enum2value(TSTS.SESSIONFLAGS, sessdata["LSMSessionInfoExPtr"]["LSM_SessionInfo_Level1"]["SessionFlags"])
-                    self.sessions[SessionId]["flags"] = sessflags
+                    sessions[SessionId]["flags"] = sessflags
                     domain = sessdata["LSMSessionInfoExPtr"]["LSM_SessionInfo_Level1"]["DomainName"]
-                    if not len(self.sessions[SessionId]["Domain"]) and len(domain):
-                        self.sessions[SessionId]["Domain"] = domain
+                    if not len(sessions[SessionId]["Domain"]) and len(domain):
+                        sessions[SessionId]["Domain"] = domain
                     username = sessdata["LSMSessionInfoExPtr"]["LSM_SessionInfo_Level1"]["UserName"]
-                    if not len(self.sessions[SessionId]["Username"]) and len(username):
-                        self.sessions[SessionId]["Username"] = username
-                    self.sessions[SessionId]["ConnectTime"] = sessdata["LSMSessionInfoExPtr"]["LSM_SessionInfo_Level1"]["ConnectTime"]
-                    self.sessions[SessionId]["DisconnectTime"] = sessdata["LSMSessionInfoExPtr"]["LSM_SessionInfo_Level1"]["DisconnectTime"]
-                    self.sessions[SessionId]["LogonTime"] = sessdata["LSMSessionInfoExPtr"]["LSM_SessionInfo_Level1"]["LogonTime"]
-                    self.sessions[SessionId]["LastInputTime"] = sessdata["LSMSessionInfoExPtr"]["LSM_SessionInfo_Level1"]["LastInputTime"]
+                    if not len(sessions[SessionId]["Username"]) and len(username):
+                        sessions[SessionId]["Username"] = username
+                    sessions[SessionId]["ConnectTime"] = sessdata["LSMSessionInfoExPtr"]["LSM_SessionInfo_Level1"]["ConnectTime"]
+                    sessions[SessionId]["DisconnectTime"] = sessdata["LSMSessionInfoExPtr"]["LSM_SessionInfo_Level1"]["DisconnectTime"]
+                    sessions[SessionId]["LogonTime"] = sessdata["LSMSessionInfoExPtr"]["LSM_SessionInfo_Level1"]["LogonTime"]
+                    sessions[SessionId]["LastInputTime"] = sessdata["LSMSessionInfoExPtr"]["LSM_SessionInfo_Level1"]["LastInputTime"]
 
     @requires_admin
     def qwinsta(self):
@@ -881,22 +882,22 @@ class smb(connection):
             "WTS_SESSIONSTATE_LOCK": "Locked",
             "WTS_SESSIONSTATE_UNLOCK": "Unlocked",
         }
-        self.get_session_list()
-        if not len(self.sessions):
+        sessions = self.get_session_list()
+        if not len(sessions):
             return
-        self.enumerate_sessions_info()
+        self.enumerate_sessions_info(sessions)
 
-        maxSessionNameLen = max([len(self.sessions[i]["SessionName"])+1 for i in self.sessions])
+        maxSessionNameLen = max([len(sessions[i]["SessionName"])+1 for i in sessions])
         maxSessionNameLen = maxSessionNameLen if len("SESSIONNAME") < maxSessionNameLen else len("SESSIONNAME")+1
-        maxUsernameLen = max([len(self.sessions[i]["Username"]+self.sessions[i]["Domain"])+1 for i in self.sessions])+1
+        maxUsernameLen = max([len(sessions[i]["Username"]+sessions[i]["Domain"])+1 for i in sessions])+1
         maxUsernameLen = maxUsernameLen if len("Username") < maxUsernameLen else len("Username")+1
-        maxIdLen = max([len(str(i)) for i in self.sessions])
+        maxIdLen = max([len(str(i)) for i in sessions])
         maxIdLen = maxIdLen if len("ID") < maxIdLen else len("ID")+1
-        maxStateLen = max([len(self.sessions[i]["state"])+1 for i in self.sessions])
+        maxStateLen = max([len(sessions[i]["state"])+1 for i in sessions])
         maxStateLen = maxStateLen if len("STATE") < maxStateLen else len("STATE")+1
-        maxRemoteIp = max([len(self.sessions[i]["RemoteIp"])+1 for i in self.sessions])
+        maxRemoteIp = max([len(sessions[i]["RemoteIp"])+1 for i in sessions])
         maxRemoteIp = maxRemoteIp if len("RemoteAddress") < maxRemoteIp else len("RemoteAddress")+1
-        maxClientName = max([len(self.sessions[i]["ClientName"])+1 for i in self.sessions])
+        maxClientName = max([len(sessions[i]["ClientName"])+1 for i in sessions])
         maxClientName = maxClientName if len("ClientName") < maxClientName else len("ClientName")+1
         template = ("{SESSIONNAME: <%d} "
                     "{USERNAME: <%d} "
@@ -931,20 +932,20 @@ class smb(connection):
         header2_verbose = ""
         result.extend((header + header_verbose, header2 + header2_verbose + "\n"))
 
-        for i in self.sessions:
-            connectTime = self.sessions[i]["ConnectTime"]
+        for i in sessions:
+            connectTime = sessions[i]["ConnectTime"]
             connectTime = connectTime.strftime(r"%Y/%m/%d %H:%M:%S") if connectTime.year > 1601 else "None"
 
-            disconnectTime = self.sessions[i]["DisconnectTime"]
+            disconnectTime = sessions[i]["DisconnectTime"]
             disconnectTime = disconnectTime.strftime(r"%Y/%m/%d %H:%M:%S") if disconnectTime.year > 1601 else "None"
-            userName = self.sessions[i]["Domain"] + "\\" + self.sessions[i]["Username"] if len(self.sessions[i]["Username"]) else ""
+            userName = sessions[i]["Domain"] + "\\" + sessions[i]["Username"] if len(sessions[i]["Username"]) else ""
 
             row = template.format(
-                SESSIONNAME=self.sessions[i]["SessionName"],
+                SESSIONNAME=sessions[i]["SessionName"],
                 USERNAME=userName,
                 ID=i,
-                STATE=self.sessions[i]["state"],
-                DSTATE=desktop_states[self.sessions[i]["flags"]],
+                STATE=sessions[i]["state"],
+                DSTATE=desktop_states[sessions[i]["flags"]],
                 CONNTIME=connectTime,
                 DISCTIME=disconnectTime,
             )
@@ -960,20 +961,20 @@ class smb(connection):
         with TSTS.LegacyAPI(self.conn, self.host, self.kerberos) as legacy:
             try:
                 handle = legacy.hRpcWinStationOpenServer()
-                r = legacy.hRpcWinStationGetAllProcesses(handle)
-            except:
+                res = legacy.hRpcWinStationGetAllProcesses(handle)
+            except Exception as e:
                 # TODO: Issue https://github.com/fortra/impacket/issues/1816
-                self.logger.debug("Exception while calling hRpcWinStationGetAllProcesses")
+                self.logger.debug(f"Exception while calling hRpcWinStationGetAllProcesses: {e}")
                 return
-            if not len(r):
+            if not res:
                 return
             self.logger.success("Enumerated processes")
-            maxImageNameLen = max([len(i["ImageName"]) for i in r])
-            maxSidLen = max([len(i["pSid"]) for i in r])
+            maxImageNameLen = max([len(i["ImageName"]) for i in res])
+            maxSidLen = max([len(i["pSid"]) for i in res])
             template = "{: <%d} {: <8} {: <11} {: <%d} {: >12}" % (maxImageNameLen, maxSidLen)
             self.logger.highlight(template.format("Image Name", "PID", "Session#", "SID", "Mem Usage"))
             self.logger.highlight(template.replace(": ", ":=").format("", "", "", "", ""))
-            for procInfo in r:
+            for procInfo in res:
                 row = template.format(
                     procInfo["ImageName"],
                     procInfo["UniqueProcessId"],
