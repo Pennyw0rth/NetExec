89b3939b01a804d3659de744850b373fd1d32051
4nh4ck1ne <leandre.star@gmail.com>
2025-03-26 12:26:21 +0100

    update - ruff check

--- nxc/modules/restrictedadmin.py
+++ nxc/modules/restrictedadmin.py
@@ -1,3 +1,4 @@
+import sys
 from impacket.dcerpc.v5 import rrp
 from impacket.examples.secretsdump import RemoteOperations
 
@@ -21,43 +22,43 @@ class NXCModule:
         ACTION:     "read" or "enable" or "disable" 
         '''
         self.action = None
-        if not 'ACTION' in module_options:
-            self.action = 'read' # DEFAULT MODE
-        if 'ACTION' in module_options:
-            if module_options['ACTION'] == 'enable':
-                self.action = 'enable'
-            if module_options['ACTION'] == 'disable':
-                self.action = 'disable'
-            if module_options['ACTION'] == 'read':
-                self.action = 'read'
+        if "ACTION" not in module_options:
+            self.action = "read" # DEFAULT MODE
+        if "ACTION" in module_options:
+            if module_options["ACTION"] == "enable":
+                self.action = "enable"
+            if module_options["ACTION"] == "disable":
+                self.action = "disable"
+            if module_options["ACTION"] == "read":
+                self.action = "read"
 
     def on_admin_login(self, context, connection):
 
-        if self.action == 'read':
+        if self.action == "read":
             # READ MODE
             read = self.check_status(context, connection)
-            if read == 1 : 
-                context.log.fail(f'DisableRestrictedAdmin key is set to 0x{read}, PTH on RDP is not allowed.')
-            if read == 0 :
-                context.log.highlight(f'DisableRestrictedAdmin key is set to 0x{read}, PTH on RDP is allowed.')
+            if read == 1: 
+                context.log.fail(f"DisableRestrictedAdmin key is set to 0x{read}, PTH on RDP is not allowed.")
+            if read == 0:
+                context.log.highlight(f"DisableRestrictedAdmin key is set to 0x{read}, PTH on RDP is allowed.")
             if read != 0 and read != 1:
-                context.log.error('Error unknown value on regkey : %s' % str(read)) 
-        if self.action == 'enable':
+                context.log.error("Error unknown value on regkey : %s" % str(read)) 
+        if self.action == "enable":
             # ENABLE MODE
             enable = self.enable(context, connection)
-            if enable == True :
+            if enable is True:
                 if self.check_status(context, connection) == 0:
-                    context.log.highlight('Operation completed successfully.')
+                    context.log.highlight("Operation completed successfully.")
                 else:
-                    context.log.error('Error unknown value on regkey : %s' % str(read))
-        if self.action == 'disable':
+                    context.log.error("Error unknown value on regkey : %s" % str(read))
+        if self.action == "disable":
             # DISABLE MODE
             disable = self.disable(context, connection)
-            if disable == True :
+            if disable is True:
                 if self.check_status(context, connection) == 1:
-                    context.log.highlight('Operation completed successfully.')
+                    context.log.highlight("Operation completed successfully.")
                 else:
-                    context.log.error('Error unknown value on regkey : %s' % str(read))
+                    context.log.error("Error unknown value on regkey : %s" % str(read))
 
 
     def check_status(self, context, connection):
@@ -78,11 +79,11 @@ class NXCModule:
                 query = rrp.hBaseRegQueryValue(remoteOps._RemoteOperations__rrp, keyHandle, "DisableRestrictedAdmin\x00")
                 return query[1]                
             except Exception as e:
-                context.log.error('RemoteOperations failed: %s' % str(e))
+                context.log.error("RemoteOperations failed: %s" % str(e))
                 sys.exit(1) 
 
         except Exception as e:
-                context.log.error('RemoteOperations failed: %s' % str(e))
+                context.log.error("RemoteOperations failed: %s" % str(e))
                 sys.exit(1)
 
         finally:
@@ -93,7 +94,7 @@ class NXCModule:
     def enable(self, context, connection):
         
         try:
-            remoteOps  = RemoteOperations(connection.conn, False)
+            remoteOps = RemoteOperations(connection.conn, False)
             remoteOps.enableRegistry()
 
             # HKLM Access
@@ -105,14 +106,14 @@ class NXCModule:
             keyHandle = rrp.hBaseRegOpenKey(remoteOps._RemoteOperations__rrp, regHandle, registry_path)["phkResult"]
 
             try:
-                query = rrp.hBaseRegSetValue(remoteOps._RemoteOperations__rrp, keyHandle, 'DisableRestrictedAdmin\x00', rrp.REG_DWORD, 0)
+                rrp.hBaseRegSetValue(remoteOps._RemoteOperations__rrp, keyHandle, "DisableRestrictedAdmin\x00", rrp.REG_DWORD, 0)
                 return True                
             except Exception as e:
-                context.log.error('RemoteOperations failed: %s' % str(e))
+                context.log.error("RemoteOperations failed: %s" % str(e))
                 sys.exit(1) 
 
         except Exception as e:
-                context.log.error('RemoteOperations failed: %s' % str(e))
+                context.log.error("RemoteOperations failed: %s" % str(e))
                 sys.exit(1)
 
         finally:
@@ -123,7 +124,7 @@ class NXCModule:
     def disable(self, context, connection):
         
         try:
-            remoteOps  = RemoteOperations(connection.conn, False)
+            remoteOps = RemoteOperations(connection.conn, False)
             remoteOps.enableRegistry()
 
             # HKLM Access
@@ -135,14 +136,14 @@ class NXCModule:
             keyHandle = rrp.hBaseRegOpenKey(remoteOps._RemoteOperations__rrp, regHandle, registry_path)["phkResult"]
 
             try:
-                query = rrp.hBaseRegSetValue(remoteOps._RemoteOperations__rrp, keyHandle, 'DisableRestrictedAdmin\x00', rrp.REG_DWORD, 1)
+                rrp.hBaseRegSetValue(remoteOps._RemoteOperations__rrp, keyHandle, "DisableRestrictedAdmin\x00", rrp.REG_DWORD, 1)
                 return True                
             except Exception as e:
-                context.log.error('RemoteOperations failed: %s' % str(e))
+                context.log.error("RemoteOperations failed: %s" % str(e))
                 sys.exit(1) 
 
         except Exception as e:
-                context.log.error('RemoteOperations failed: %s' % str(e))
+                context.log.error("RemoteOperations failed: %s" % str(e))
                 sys.exit(1)
 
         finally:
