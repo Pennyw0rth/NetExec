95c11aa43489fc23b0c1f14e7148ec5528584f75
D4ytox <d4ytox@protonmail.com>
2025-04-18 14:17:32 +0100

    Use NXC_PATH,CONFIG_PATH in context.py

--- nxc/context.py
+++ nxc/context.py
@@ -1,6 +1,8 @@
 import configparser
 import os
 
+from nxc.paths import NXC_PATH, CONFIG_PATH
+
 
 class Context:
     def __init__(self, db, logger, args):
@@ -8,10 +10,10 @@ class Context:
             setattr(self, key, value)
 
         self.db = db
-        self.log_folder_path = os.path.join(os.path.expanduser("~/.nxc"), "logs")
+        self.log_folder_path = os.path.join(os.path.expanduser(NXC_PATH), "logs")
         self.localip = None
 
         self.conf = configparser.ConfigParser()
-        self.conf.read(os.path.expanduser("~/.nxc/nxc.conf"))
+        self.conf.read(CONFIG_PATH)
 
         self.log = logger
