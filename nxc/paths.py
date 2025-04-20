ff1ce969f9485c4099027832169447e063521498
D4ytox <d4ytox@protonmail.com>
2025-04-20 14:49:25 +0100

    Use XDG_CONFIG_HOME and XDG_DATA_HOME if set by user

--- nxc/paths.py
+++ nxc/paths.py
@@ -2,7 +2,7 @@ import os
 import sys
 import nxc
 
-NXC_PATH = os.path.expanduser("~/.nxc")
+NXC_PATH = os.getenv('XDG_CONFIG_HOME', os.path.expanduser("~/.nxc"))
 if os.name == "nt":
     TMP_PATH = os.getenv("LOCALAPPDATA") + "\\Temp\\nxc_hosted"
 elif hasattr(sys, "getandroidapilevel"):
@@ -13,4 +13,4 @@ else:
 CERT_PATH = os.path.join(NXC_PATH, "nxc.pem")
 CONFIG_PATH = os.path.join(NXC_PATH, "nxc.conf")
 WORKSPACE_DIR = os.path.join(NXC_PATH, "workspaces")
-DATA_PATH = os.path.join(os.path.dirname(nxc.__file__), "data")
+DATA_PATH = os.getenv('XDG_DATA_HOME', os.path.join(os.path.dirname(nxc.__file__), "data"))
