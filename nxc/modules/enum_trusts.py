ed88beebf9daed1873c0868627f06ec48ab33e68
termanix <mehmetcantopal9@hotmail.com>
2025-05-28 09:56:21 -0400

    added remove sign to description on enum_trust

--- nxc/modules/enum_trusts.py
+++ nxc/modules/enum_trusts.py
@@ -7,7 +7,7 @@ class NXCModule:
     """
 
     name = "enum_trusts"
-    description = "Extract all Trust Relationships, Trusting Direction, and Trust Transitivity"
+    description = "[REMOVED] Extract all Trust Relationships, Trusting Direction, and Trust Transitivity"
     supported_protocols = ["ldap"]
     opsec_safe = True
     multiple_hosts = True
