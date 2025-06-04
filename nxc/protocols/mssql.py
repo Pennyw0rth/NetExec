c4f5c003be4254c78a5b6c2fc2168d49116be031
Aurélien CHALOT <aurelien.chalot@orangecyberdefense.com>
2025-06-04 13:40:46 +0200

    Ruff level up + remove useless code

--- nxc/protocols/mssql.py
+++ nxc/protocols/mssql.py
@@ -6,8 +6,6 @@ import contextlib
 from io import StringIO
 from termcolor import colored
 
-
-from nxc.config import process_secret
 from nxc.connection import connection
 from nxc.connection import requires_admin
 from nxc.logger import NXCAdapter
@@ -36,12 +34,10 @@ from impacket.tds import (
 
 # Added by @Deft to support version enum and encryption via PRELOGIN
 from impacket.tds import (
-    TDS_PRELOGIN,        # Used to structure to initialize first packet to MSSQL
-    TDS_ENCRYPT_OFF,     # Encryption is available but off
-    TDS_ENCRYPT_ON,      # Encryption is available and on
-    TDS_ENCRYPT_NOT_SUP, # Encryption is not available
-    TDS_ENCRYPT_REQ      # Encryption is required
+    TDS_PRELOGIN,         # Used to structure to initialize first packet to MSSQL
+    TDS_ENCRYPT_NOT_SUP,  # Encryption is not available
 )
+
 
 # This was taken from https://github.com/CompassSecurity/mssqlrelay/blob/main/mssqlrelay/commands/check.py 
 # And added by @Deft_ to support version enum, thanks @sploutchy for the work!!
@@ -49,186 +45,173 @@ class MSSQL_VERSION:
     # from https://sqlserverbuilds.blogspot.com/
     VERSION_NAME = ("Microsoft SQL Server", {
         # Out of support
-        6 : ("6", {
-            0 : (".0", {
-                121 : "RTM (no SP)",
-                124 : "(SP1)",
-                139 : "(SP2)",
-                151 : "(SP3)",
+        6: ("6", {
+            0: (".0", {
+                121: "RTM (no SP)",
+                124: "(SP1)",
+                139: "(SP2)",
+                151: "(SP3)",
             }),
-            50 : (".5", {
-                201 : "RTM (no SP)",
-                213 : "(SP1)",
-                240 : "(SP2)",
-                258 : "(SP3)",
-                281 : "(SP4)",
-                416 : "(SP5)",
-            }),
-        }),
-        7 : ("7", {
-            0 : ("", {
-                623 : "RTM (no SP)",
-                699 : "(SP1)",
-                842 : "(SP2)",
-                961 : "(SP3)",
-                1063 : "(SP4)",
+            50: (".5", {
+                201: "RTM (no SP)",
+                213: "(SP1)",
+                240: "(SP2)",
+                258: "(SP3)",
+                281: "(SP4)",
+                416: "(SP5)",
             }),
         }),
-        8 : ("2000", {
-            0 : ("", {
-                194 : "RTM (no SP)",
-                384 : "(SP1)",
-                532 : "(SP2)",
-                760 : "(SP3)",
-                2039 : "(SP4)",
+        7: ("7", {
+            0: ("", {
+                623: "RTM (no SP)",
+                699: "(SP1)",
+                842: "(SP2)",
+                961: "(SP3)",
+                1063: "(SP4)",
             }),
         }),
-        9 : ("2005", {
-            0 : ("", {
-                1399 : "RTM (no SP)",
-                2047 : "(SP1)",
-                3042 : "(SP2)",
-                4035 : "(SP3)",
-                5000 : "(SP4)",
+        8: ("2000", {
+            0: ("", {
+                194: "RTM (no SP)",
+                384: "(SP1)",
+                532: "(SP2)",
+                760: "(SP3)",
+                2039: "(SP4)",
             }),
         }),
-        10 : ("2008", {
-            0 : ("", {
-                1600 : "RTM (no SP)",
-                2531 : "(SP1)",
-                4000 : "(SP2)",
-                5500 : "(SP3)",
-                6000 : "(SP4)",
-            }),
-            50 : (" R2", {
-                1600 : "RTM (no SP)",
-                2500 : "(SP1)",
-                4000 : "(SP2)",
-                6000 : "(SP3)",
+        9: ("2005", {
+            0: ("", {
+                1399: "RTM (no SP)",
+                2047: "(SP1)",
+                3042: "(SP2)",
+                4035: "(SP3)",
+                5000: "(SP4)",
             }),
         }),
-        11 : ("2012", {
-            0 : ("", {
-                2100 : "RTM (no SP)",
-                3000 : "(SP1)",
-                5058 : "(SP2)",
-                6020 : "(SP3)",
-                7001 : "(SP4)",
+        10: ("2008", {
+            0: ("", {
+                1600: "RTM (no SP)",
+                2531: "(SP1)",
+                4000: "(SP2)",
+                5500: "(SP3)",
+                6000: "(SP4)",
+            }),
+            50: (" R2", {
+                1600: "RTM (no SP)",
+                2500: "(SP1)",
+                4000: "(SP2)",
+                6000: "(SP3)",
+            }),
+        }),
+        11: ("2012", {
+            0: ("", {
+                2100: "RTM (no SP)",
+                3000: "(SP1)",
+                5058: "(SP2)",
+                6020: "(SP3)",
+                7001: "(SP4)",
             }),
         }),
         # Supported
-        12 : ("2014", {
-            0 : ("", {
-                2000 : "RTM (no SP)",
-                4100 : "(SP1)",
-                5000 : "(SP2)",
-                6024 : "(SP3)",
+        12: ("2014", {
+            0: ("", {
+                2000: "RTM (no SP)",
+                4100: "(SP1)",
+                5000: "(SP2)",
+                6024: "(SP3)",
             }),
         }),
-        13 : ("2016", {
-            0 : ("", {
-                1601 : "RTM (no SP)",
-                4001 : "(SP1)",
-                5026 : "(SP2)",
-                6300 : "(SP3)",
+        13: ("2016", {
+            0: ("", {
+                1601: "RTM (no SP)",
+                4001: "(SP1)",
+                5026: "(SP2)",
+                6300: "(SP3)",
             }),
         }),
-        14 : ("2017", {
-            0 : ("", {
-                1000 : "RTM",
-                3006 : "(CU1)",
-                3008 : "(CU2)",
-                3015 : "(CU3)",
-                3022 : "(CU4)",
-                3023 : "(CU5)",
-                3025 : "(CU6)",
-                3026 : "(CU7)",
-                3029 : "(CU8)",
-                3030 : "(CU9)",
-                3037 : "(CU10)",
-                3038 : "(CU11)",
-                3045 : "(CU12)",
-                3048 : "(CU13)",
-                3076 : "(CU14)",
-                3162 : "(CU15)",
-                3223 : "(CU16)",
-                3228 : "(CU17)",
-                3257 : "(CU18)",
-                3281 : "(CU19)",
-                3294 : "(CU20)",
-                3335 : "(CU21)",
-                3356 : "(CU22)",
-                3381 : "(CU23)",
-                3391 : "(CU24)",
-                3401 : "(CU25)",
-                3411 : "(CU26)",
-                3421 : "(CU27)",
-                3430 : "(CU28)",
-                3436 : "(CU29)",
-                3451 : "(CU30)",
-                3456 : "(CU31)",
+        14: ("2017", {
+            0: ("", {
+                1000: "RTM",
+                3006: "(CU1)",
+                3008: "(CU2)",
+                3015: "(CU3)",
+                3022: "(CU4)",
+                3023: "(CU5)",
+                3025: "(CU6)",
+                3026: "(CU7)",
+                3029: "(CU8)",
+                3030: "(CU9)",
+                3037: "(CU10)",
+                3038: "(CU11)",
+                3045: "(CU12)",
+                3048: "(CU13)",
+                3076: "(CU14)",
+                3162: "(CU15)",
+                3223: "(CU16)",
+                3228: "(CU17)",
+                3257: "(CU18)",
+                3281: "(CU19)",
+                3294: "(CU20)",
+                3335: "(CU21)",
+                3356: "(CU22)",
+                3381: "(CU23)",
+                3391: "(CU24)",
+                3401: "(CU25)",
+                3411: "(CU26)",
+                3421: "(CU27)",
+                3430: "(CU28)",
+                3436: "(CU29)",
+                3451: "(CU30)",
+                3456: "(CU31)",
             }),
         }),
-        15 : ("2019", {
-            0 : ("", {
-                2000 : "RTM",
-                4003 : "(CU1)",
-                4013 : "(CU2)",
-                4023 : "(CU3)",
-                4033 : "(CU4)",
-                4043 : "(CU5)",
-                4053 : "(CU6)",
-                4063 : "(CU7)",
-                4073 : "(CU8)",
-                4102 : "(CU9)",
-                4123 : "(CU10)",
-                4138 : "(CU11)",
-                4153 : "(CU12)",
-                4178 : "(CU13)",
-                4188 : "(CU14)",
-                4198 : "(CU15)",
-                4223 : "(CU16)",
-                4249 : "(CU17)",
-                4261 : "(CU18)",
-                4298 : "(CU19)",
-                4312 : "(CU20)",
+        15: ("2019", {
+            0: ("", {
+                2000: "RTM",
+                4003: "(CU1)",
+                4013: "(CU2)",
+                4023: "(CU3)",
+                4033: "(CU4)",
+                4043: "(CU5)",
+                4053: "(CU6)",
+                4063: "(CU7)",
+                4073: "(CU8)",
+                4102: "(CU9)",
+                4123: "(CU10)",
+                4138: "(CU11)",
+                4153: "(CU12)",
+                4178: "(CU13)",
+                4188: "(CU14)",
+                4198: "(CU15)",
+                4223: "(CU16)",
+                4249: "(CU17)",
+                4261: "(CU18)",
+                4298: "(CU19)",
+                4312: "(CU20)",
             }),
         }),
-        16 : ("2022", {
-            0 : ("", {
-                1000 : "RTM",
-                4003 : "(CU1)",
-                4015 : "(CU2)",
-                4025 : "(CU3)",
-                4035 : "(CU4)",
+        16: ("2022", {
+            0: ("", {
+                1000: "RTM",
+                4003: "(CU1)",
+                4015: "(CU2)",
+                4025: "(CU3)",
+                4035: "(CU4)",
             }),
         }),
     })
 
     def __init__(self, version):
-        self.major, self.minor, self.build = struct.unpack_from('>bbH', version)
+        self.major, self.minor, self.build = struct.unpack_from(">bbH", version)
 
     @property
     def version_number(self):
-        return "%i.%i.%i" % (self.major, self.minor, self.build)
-
-    @property
-    def version_name(self):
-        try:
-            string = MSSQL_VERSION.VERSION_NAME[0]
-            string += " "
-            string += MSSQL_VERSION.VERSION_NAME[1][self.major][0]
-            string += MSSQL_VERSION.VERSION_NAME[1][self.major][1][self.minor][0]
-            string += " "
-            string += MSSQL_VERSION.VERSION_NAME[1][self.major][1][self.minor][1][self.build]
-        except KeyError:
-            string += "(unknown)"
-        finally:
-            return string
+        return f"{self.major}.{self.minor}.{self.build}"
 
     def __repr__(self):
         return self.version_number
     
+
 class mssql(connection):
     def __init__(self, args, db, host):
         self.mssql_instances = []
@@ -293,17 +276,17 @@ class mssql(connection):
     def check_encryption(self):
         try:
             prelogin = TDS_PRELOGIN()
-            prelogin['Version'] = b"\x08\x00\x01\x55\x00\x00"
-            prelogin['Encryption'] = TDS_ENCRYPT_NOT_SUP
-            prelogin['ThreadID'] = struct.pack('<L', random.randint(0, 65535))
-            prelogin['Instance'] = b'MSSQLServer\x00'
+            prelogin["Version"] = b"\x08\x00\x01\x55\x00\x00"
+            prelogin["Encryption"] = TDS_ENCRYPT_NOT_SUP
+            prelogin["ThreadID"] = struct.pack("<L", random.randint(0, 65535))
+            prelogin["Instance"] = b"MSSQLServer\x00"
             self.conn.sendTDS(TDS_PRE_LOGIN, prelogin.getData(), 0)
             tds = self.conn.recvTDS()
-            response = TDS_PRELOGIN(tds['Data'])
-            version = MSSQL_VERSION(response['Version'])
-            return version, response['Encryption']
+            response = TDS_PRELOGIN(tds["Data"])
+            version = MSSQL_VERSION(response["Version"])
+            return version, response["Encryption"]
         except Exception as e:
-            self.logger.error(f"Exception in mssql:check_encryption {str(e)}")
+            self.logger.error(f"Exception in mssql:check_encryption {e!s}")
             return "", ""
     
     @reconnect_mssql
