7ea164af9798329a1778eb28eac482ec03add12f
Alex <61382599+NeffIsBack@users.noreply.github.com>
2025-06-10 12:47:02 +0200

    Normpath for nxc folder

--- nxc/paths.py
+++ nxc/paths.py
@@ -5,7 +5,7 @@ import nxc
 if "XDG_CONFIG_HOME" in os.environ:  # noqa: SIM108
     NXC_PATH = os.path.join(os.getenv("XDG_CONFIG_HOME"), "nxc")
 else:
-    NXC_PATH = os.path.expanduser("~/.nxc")
+    NXC_PATH = os.path.normpath(os.path.expanduser("~/.nxc"))
     
 if os.name == "nt":
     TMP_PATH = os.getenv("LOCALAPPDATA") + "\\Temp\\nxc_hosted"
