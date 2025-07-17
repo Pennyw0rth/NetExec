b31f23bc542d945da6632de67d842adfe878a8a7
Aurélien CHALOT <aurelien.chalot@orangecyberdefense.com>
2025-07-17 10:42:50 +0200

    Remove staled code 'fileless' since we removed the integrated SMB server and provided no option for it

--- nxc/protocols/smb/atexec.py
+++ nxc/protocols/smb/atexec.py
@@ -4,7 +4,6 @@ from impacket.dcerpc.v5 import tsch, transport
 from impacket.dcerpc.v5.dtypes import NULL
 from impacket.dcerpc.v5.rpcrt import RPC_C_AUTHN_GSS_NEGOTIATE, RPC_C_AUTHN_LEVEL_PKT_PRIVACY
 from nxc.helpers.misc import gen_random_string
-from nxc.paths import TMP_PATH
 from time import sleep
 from datetime import datetime, timedelta
 
@@ -32,6 +31,7 @@ class TSCH_EXEC:
         self.__output_filename = None
         self.__share = share
         self.logger = logger
+        self.smbConnection = None
         self.upload_task_binary = upload_task_binary
         # Specifies that the task should be run as SYSTEM (S-1-5-18) or the run_task_as username
         self.run_task_as = "S-1-5-18" if run_task_as is None else run_task_as
@@ -82,7 +82,7 @@ class TSCH_EXEC:
         # Format it to match the format in the XML: "YYYY-MM-DDTHH:MM:SS.ssssss"
         return end_boundary.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
 
-    def gen_xml(self, command, fileless=False):
+    def gen_xml(self, command):
         xml = f"""<?xml version="1.0" encoding="UTF-16"?>
         <Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
         <Triggers>
@@ -120,11 +120,7 @@ class TSCH_EXEC:
         """
         if self.__retOutput:
             self.__output_filename = f"{self.temp_dir}\{gen_random_string(6)}"
-            if fileless:
-                local_ip = self.__rpctransport.get_socket().getsockname()[0]
-                argument_xml = f"      <Arguments>/C {command} &gt; \\\\{local_ip}\\{self.__share_name}\\{self.__output_filename} 2&gt;&amp;1</Arguments>"
-            else:
-                argument_xml = f"      <Arguments>/C {command} &gt; {self.__output_filename} 2&gt;&amp;1</Arguments>"
+            argument_xml = f"      <Arguments>/C {command} &gt; {self.__output_filename} 2&gt;&amp;1</Arguments>"
 
         elif self.__retOutput is False:
             argument_xml = f"      <Arguments>/C {command}</Arguments>"
@@ -140,7 +136,7 @@ class TSCH_EXEC:
         # Removing identation in the final computed XML file
         return dedent(xml)
 
-    def execute_handler(self, command, fileless=False):
+    def execute_handler(self, command):
         dce = self.__rpctransport.get_dce_rpc()
         if self.__doKerberos:
             dce.set_auth_type(RPC_C_AUTHN_GSS_NEGOTIATE)
@@ -149,7 +145,7 @@ class TSCH_EXEC:
         dce.connect()
 
         # Creates a fully working SMB connection used to upload/delete files
-        smbConnection = self.__rpctransport.get_smb_connection()
+        self.smbConnection = self.__rpctransport.get_smb_connection()
 
         # Uploads the binary that will be run via the scheduled task
         if self.upload_task_binary is not None:
@@ -160,16 +156,15 @@ class TSCH_EXEC:
                 binary_name = os.path.basename(upload_task_binary)
                 # Uploads the file to \\Windows\Temp\binary.exe
                 with open(upload_task_binary, "rb") as binary_content:
-                    smbConnection.putFile(self.__share, f"{self.temp_dir}{binary_name}", binary_content.read)
+                    self.smbConnection.putFile(self.__share, f"{self.temp_dir}{binary_name}", binary_content.read)
                 # Modifies the command to run to include full path
                 command = f"{self.temp_dir}{command}"
             else:
                 self.logger.fail(f"Cannot open {self.upload_task_binary}, canceling task creation")
                 return
 
-        xml = self.gen_xml(command, fileless)
-        self.logger.debug(f"Task XML: {xml}")
-        self.logger.info(f"Creating task \\{self.task_name}")
+        xml = self.gen_xml(command)
+        self.logger.debug(f"Creating task \\{self.task_name} with XML: {xml}")
         try:
             # windows server 2003 has no MSRPC_UUID_TSCHS, if it bind, it will return abstract_syntax_not_supported
             dce.set_auth_level(RPC_C_AUTHN_LEVEL_PKT_PRIVACY)
@@ -198,63 +193,52 @@ class TSCH_EXEC:
         tsch.hSchRpcDelete(dce, f"\\{self.task_name}")
 
         if self.__retOutput:
-            if fileless:
-                while True:
-                    try:
-                        with open(os.path.join(TMP_PATH, self.__output_filename)) as output:
-                            self.output_callback(output.read())
-                        break
-                    except OSError:
-                        sleep(2)
-            else:
-                ":".join(map(str, self.__rpctransport.get_socket().getpeername()))
-                smbConnection = self.__rpctransport.get_smb_connection()
-
-                tries = 1
-                # Give the command a bit of time to execute before we try to read the output, 0.4 seconds was good in testing
-                sleep(0.4)
-                while True:
-                    try:
-                        self.logger.info(f"Attempting to read {self.__share}\\{self.__output_filename}")
-                        smbConnection.getFile(self.__share, self.__output_filename, self.output_callback)
-                        break
-                    except Exception as e:
-                        if tries >= self.__tries:
-                            self.logger.fail("ATEXEC: Could not retrieve output file, it may have been detected by AV. Please increase the number of tries with the option '--get-output-tries'. If it is still failing, try the 'wmi' protocol or another exec method")
-                            break
-                        if "STATUS_BAD_NETWORK_NAME" in str(e):
-                            self.logger.fail(f"ATEXEC: Getting the output file failed - target has blocked access to the share: {self.__share} (but the command may have executed!)")
-                            break
-                        elif "STATUS_VIRUS_INFECTED" in str(e):
-                            self.logger.fail("Command did not run because a virus was detected")
-                            break
-                        # When executing powershell and the command is still running, we get a sharing violation
-                        # We can use that information to wait longer than if the file is not found (probably av or something)
-                        if "STATUS_SHARING_VIOLATION" in str(e):
-                            self.logger.info(f"File {self.__share}\\{self.__output_filename} is still in use with {self.__tries - tries} tries left, retrying...")
-                            tries += 1
-                            sleep(1)
-                        elif "STATUS_OBJECT_NAME_NOT_FOUND" in str(e):
-                            self.logger.info(f"File {self.__share}\\{self.__output_filename} not found with {self.__tries - tries} tries left, deducting 10 tries and retrying...")
-                            tries += 10
-                            sleep(1)
-                        else:
-                            self.logger.debug(f"Exception when trying to read output file: {e!s}. {self.__tries - tries} tries left, retrying...")
-                            tries += 1
-                            sleep(1)
-
+            tries = 1
+            # Give the command a bit of time to execute before we try to read the output, 0.4 seconds was good in testing
+            sleep(0.4)
+            while True:
                 try:
-                    self.logger.debug(f"Deleting file {self.__share}\\{self.__output_filename}")
-                    smbConnection.deleteFile(self.__share, self.__output_filename)
+                    self.logger.info(f"Attempting to read {self.__share}\\{self.__output_filename}")
+                    self.smbConnection.getFile(self.__share, self.__output_filename, self.output_callback)
+                    break
                 except Exception as e:
-                    self.logger.fail(f"Error while deleting file {self.__share}\\{self.__output_filename}: {e}")
+                    if tries >= self.__tries:
+                        self.logger.fail("ATEXEC: Could not retrieve output file, it may have been detected by AV. Please increase the number of tries with the option '--get-output-tries'. If it is still failing, try the 'wmi' protocol or another exec method")
+                        break
+                    if "STATUS_BAD_NETWORK_NAME" in str(e):
+                        self.logger.fail(f"ATEXEC: Getting the output file failed - target has blocked access to the share: {self.__share} (but the command may have executed!)")
+                        break
+                    elif "STATUS_VIRUS_INFECTED" in str(e):
+                        self.logger.fail("Command did not run because a virus was detected")
+                        break
+                    # When executing powershell and the command is still running, we get a sharing violation
+                    # We can use that information to wait longer than if the file is not found (probably av or something)
+                    if "STATUS_SHARING_VIOLATION" in str(e):
+                        self.logger.info(f"File {self.__share}\\{self.__output_filename} is still in use with {self.__tries - tries} tries left, retrying...")
+                        tries += 1
+                        sleep(1)
+                    elif "STATUS_OBJECT_NAME_NOT_FOUND" in str(e):
+                        self.logger.info(f"File {self.__share}\\{self.__output_filename} not found with {self.__tries - tries} tries left, deducting 10 tries and retrying...")
+                        tries += 10
+                        sleep(1)
+                    else:
+                        self.logger.debug(f"Exception when trying to read output file: {e!s}. {self.__tries - tries} tries left, retrying...")
+                        tries += 1
+                        sleep(1)
 
-                # Removes the uploaded binary that was run via the scheduled task
-                if self.upload_task_binary is not None:
-                    try:
-                        self.logger.success(f"Binary {self.temp_dir}{binary_name} successfully deleted")
-                        smbConnection.deleteFile(self.__share, f"{self.temp_dir}{binary_name}")
-                    except Exception as e:
-                        self.logger.fail(f"Couldn't remove {self.temp_dir}{binary_name}: {e}")
+            # Removes the output response file
+            try:
+                self.logger.debug(f"Deleting file {self.__share}\\{self.__output_filename}")
+                self.smbConnection.deleteFile(self.__share, self.__output_filename)
+            except Exception as e:
+                self.logger.fail(f"Error while deleting file {self.__share}\{self.__output_filename}: {e}")
+
+            # Removes the uploaded binary that was run via the scheduled task
+            if self.upload_task_binary is not None:
+                try:
+                    self.logger.success(f"Binary {self.temp_dir}{binary_name} successfully deleted")
+                    self.smbConnection.deleteFile(self.__share, f"{self.temp_dir}{binary_name}")
+                except Exception as e:
+                    self.logger.fail(f"Couldn't remove {self.temp_dir}{binary_name}: {e}")
 
         dce.disconnect()
