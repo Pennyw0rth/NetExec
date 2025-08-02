35bc7d9a0dccf1853a13faaef378af4891aaaf3c
Marshall Hallenbeck <marshall.hallenbeck@gmail.com>
2025-08-02 10:35:29 -0400

    feat(kerberoast): add in kerberoast as alias to kerberoasting

--- nxc/protocols/ldap/proto_args.py
+++ nxc/protocols/ldap/proto_args.py
@@ -13,7 +13,7 @@ def proto_args(parser, parents):
 
     egroup = ldap_parser.add_argument_group("Retrieve hash on the remote DC", "Options to get hashes from Kerberos")
     egroup.add_argument("--asreproast", help="Output AS_REP response to crack with hashcat to file")
-    kerberoasting_arg = egroup.add_argument("--kerberoasting", help="Output TGS ticket to crack with hashcat to file")
+    kerberoasting_arg = egroup.add_argument("--kerberoasting", "--kerberoast", help="Output TGS ticket to crack with hashcat to file")
     kerberoast_users_arg = egroup.add_argument("--kerberoast-users", nargs="+", dest="kerberoast_users", action=get_conditional_action(_StoreAction), make_required=[], help="Target specific users for kerberoasting (usernames or file containing usernames)")
     egroup.add_argument("--no-preauth-targets", nargs=1, dest="no_preauth_targets", help="Targeted kerberoastable users")
     
