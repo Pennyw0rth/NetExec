9bf02253119f1a22d98ae1a6785577ede4ae76cf
haytehcy <alexhayman89@gmail.com>
2025-08-05 17:30:10 +0100

    Added e2e tests

--- nxc/modules/recyclebin.py
+++ nxc/modules/recyclebin.py
@@ -93,6 +93,6 @@ class NXCModule:
                             else:
                                 context.log.fail(f"Failed to write recyclebin file {filename}: {e}")
                 else:
-                    context.log.info('Use the module option "DOWNLOAD=True"')
+                    context.log.info('Use the module option "DOWNLOAD=True" to download the files')
 
         remote_ops.finish()
