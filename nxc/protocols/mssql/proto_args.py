18ba7dba6e37851a82969629ec02714c56e04385
Alexander Neff <alex99.neff@gmx.de>
2025-08-10 11:54:13 -0400

    Revert metavar for query

--- nxc/protocols/mssql/proto_args.py
+++ nxc/protocols/mssql/proto_args.py
@@ -8,7 +8,7 @@ def proto_args(parser, parents):
     mssql_parser.add_argument("--mssql-timeout", help="SQL server connection timeout", type=int, default=5)
 
     qgroup = mssql_parser.add_mutually_exclusive_group()
-    qgroup.add_argument("-q", "--query", dest="mssql_query", metavar="SQL", type=str, help="execute the specified query against the mssql db")
+    qgroup.add_argument("-q", "--query", dest="mssql_query", metavar="QUERY", type=str, help="execute the specified query against the mssql db")
     qgroup.add_argument("--database", nargs="?", const=True, metavar="NAME", help="list databases or list tables for NAME")
 
     dgroup = mssql_parser.add_mutually_exclusive_group()
