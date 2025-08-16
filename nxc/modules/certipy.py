913605ba221428e72f0a48fb4f5273706f799264
Alexander Neff <alex99.neff@gmx.de>
2025-08-16 08:06:12 -0400

    Rename module to 'certipy' so it is expandable in the future

--- nxc/modules/certipy.py
+++ nxc/modules/certipy.py
@@ -11,7 +11,7 @@ from nxc.paths import NXC_PATH
 
 class NXCModule:
     """Module made by: @NeffIsBack, @gatariee"""
-    name = "certipy-find"
+    name = "certipy"
     description = "certipy find command with options to export the result to text/csv/json. Default: Show only vulnerable templates"
     supported_protocols = ["ldap"]
 
