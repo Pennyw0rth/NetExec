f3a90d9aca0110278a17c2969c1f3cff8f759b45
Fabrizzio53 <88493503+Fabrizzio53@users.noreply.github.com>
2026-05-26 19:27:43 -0300

    Refactor tombstone
    refactored the code to return empty sAMAccountName when object does not have this attribute.
    
    removed some variables that were not being used, such as opsec, multiple_host and self.__domain.
    
    removed internal domain to dn function to now use connection.baseDN.
    
    the number of deleted objects should be right now, temporary var is created and incremented for each deleted object found.
    
    Signed-off-by: Fabrizzio53 <88493503+Fabrizzio53@users.noreply.github.com>

--- nxc/modules/tombstone.py
+++ nxc/modules/tombstone.py
@@ -11,8 +11,6 @@ class NXCModule:
     name = "tombstone"
     description = "Query, restore and delete AD object"
     supported_protocols = ["ldap"]
-    opsec_safe = True
-    multiple_hosts = False
     category = CATEGORY.ENUMERATION
 
     def __init__(self, context=None, module_options=None):
@@ -30,45 +28,42 @@ class NXCModule:
                nxc ldap $DC-IP -u Username -p Password -M tombstone -o ACTION=delete DN="CN=test,OU=Users,DC=test,DC=local"
                nxc ldap $DC-IP -u Username -p Password -M tombstone -o ACTION=query
         """
-        self.action = "query"
+        self.action = module_options.get("ACTION", "query")
         self.id = ""
         self.deleteDN = ""
         if "ACTION" in module_options:
-            self.action = module_options["ACTION"]
+            self.action = module_options.get("ACTION")
         if "ID" in module_options:
             self.id = module_options["ID"]
         if "DN" in module_options:
             self.deleteDN = module_options["DN"]
-        if "ACTION" in module_options and self.action == "restore" and "ID" not in module_options:
+        if "ACTION" in module_options and self.action.lower() == "restore" and "ID" not in module_options:
             context.log.error("ID is necessary when calling tombstone with the restore action")
             sys.exit(1)
 
-        if "ACTION" in module_options and self.action == "delete" and "DN" not in module_options:
+        if "ACTION" in module_options and self.action.lower() == "delete" and "DN" not in module_options:
             context.log.error("DN is necessary when calling tombstone with the delete action")
             sys.exit(1)
 
     def on_login(self, context, connection):
-        self.__domain = connection.domain
         self.__sAMAccountName = ""
         self.__objectDN = ""
         self.__lastKnownParent = ""
-        self.__domain = connection.domain
         self.connection = connection
 
-        if self.action == "query":
+        if self.action.lower() == "query":
             self.query_deleted_objects(context)
-        if self.action == "delete":
+        elif self.action.lower() == "delete":
             self.delete_object(context, connection)
-        if self.action == "restore":
+        elif self.action.lower() == "restore":
             self.restore_deleted_object(context, connection)
-
-    def domain_to_dn(self, domain):
-        return ",".join(f"DC={part}" for part in domain.split("."))
+        else:
+            context.log.fail(f'The action "{self.action}" is not valid, use only one available option (query, restore, delete)')
 
     def restore_deleted_object(self, context, connection):
 
         # ldap DN for deleted objects
-        dn = "CN=Deleted Objects," + self.domain_to_dn(self.__domain)
+        dn = f"CN=Deleted Objects,{connection.baseDN}"
 
         # LDAP control necessary to show the deleted objects LDAP_SERVER_SHOW_DELETED_OID
         show_deleted_control = Control()
@@ -87,31 +82,32 @@ class NXCModule:
 
             # This check ensures that we skip the result for the Default container and only get the result from the given ID.
             if "container" in entries["objectClass"] and entries["description"] == "Default container for deleted objects":
+
                 continue
 
             if self.id == entries["name"].split(":")[1]:
 
-                context.log.highlight("Found target!")
-                context.log.highlight(f"{'sAMAccountName':<20}: {entries['sAMAccountName']}")
-                context.log.highlight(f"{'dn':<20}: {entries['distinguishedName']}")
-                context.log.highlight(f"{'ID':<20}: {entries['name'].split(':')[1]}")
-                context.log.highlight(f"{'isDeleted':<20}: {entries['isDeleted']}")
-                context.log.highlight(f"{'lastKnownParent':<20}: {entries['lastKnownParent']}")
+                context.log.highlight(f"{'sAMAccountName':<20}: {entries.get('sAMAccountName', '')}")
+                context.log.highlight(f"{'dn':<20}: {entries.get('distinguishedName', '')}")
+                context.log.highlight(f"{'ID':<20}: {entries.get('name', '').split(':')[1]}")
+                context.log.highlight(f"{'isDeleted':<20}: {entries.get('isDeleted', '')}")
+                context.log.highlight(f"{'lastKnownParent':<20}: {entries.get('lastKnownParent', '')}")
                 context.log.highlight("")
 
-                self.__sAMAccountName = entries["sAMAccountName"]
-                self.__objectDN = entries["distinguishedName"]
-                self.__lastKnownParent = entries["lastKnownParent"]
+                self.__objectDN = entries.get("distinguishedName", "")
+                self.__lastKnownParent = entries.get("lastKnownParent", "")
+                object_prefix = self.__objectDN.split("\\")[0]
+                self.__originalDN = f"{object_prefix},{self.__lastKnownParent}"
 
                 break
 
-        if self.__sAMAccountName == "":
+        if self.__originalDN == "":
             context.log.highlight(f"The object was not found with id {self.id}.")
             return False
 
         try:
-            connection.ldap_connection.modify(dn=self.__objectDN, modifications={"isDeleted": [(MODIFY_DELETE, [])], "distinguishedName": [(MODIFY_REPLACE, [f"CN={self.__sAMAccountName},{self.__lastKnownParent}"])]}, controls=[show_deleted_control])
-            context.log.highlight(f'Success "CN={self.__sAMAccountName},{self.__lastKnownParent}" restored')
+            connection.ldap_connection.modify(dn=self.__objectDN, modifications={"isDeleted": [(MODIFY_DELETE, [])], "distinguishedName": [(MODIFY_REPLACE, [self.__originalDN])]}, controls=[show_deleted_control])
+            context.log.highlight(f"Success {self.__originalDN} restored")
 
         except LDAPSessionError as e:
             context.log.fail(f"Error at trying to recover the object {e}")
@@ -132,7 +128,7 @@ class NXCModule:
     def query_deleted_objects(self, context):
 
         # ldap DN for deleted objects
-        dn = "CN=Deleted Objects," + self.domain_to_dn(self.__domain)
+        dn = f"CN=Deleted Objects,{self.connection.baseDN}"
 
         # LDAP control necessary to show the deleted objects LDAP_SERVER_SHOW_DELETED_OID
         show_deleted_control = Control()
@@ -151,18 +147,23 @@ class NXCModule:
             context.log.highlight("No objects are in a tombstone state")
             return False
 
-        context.log.highlight(f"Found {len(resp) - 1} deleted objects")
+        number_of_deleted_objects = 0
         context.log.highlight("")
 
         for entries in resp_parsed:
 
             # This check ensures that we skip the result for the Default container and only get results that are valid for us.
             if "container" in entries["objectClass"] and entries["description"] == "Default container for deleted objects":
+
                 continue
 
-            context.log.highlight(f"{'sAMAccountName':<20}: {entries['sAMAccountName']}")
-            context.log.highlight(f"{'dn':<20}: {entries['distinguishedName']}")
-            context.log.highlight(f"{'ID':<20}: {entries['name'].split(':')[1]}")
-            context.log.highlight(f"{'isDeleted':<20}: {entries['isDeleted']}")
-            context.log.highlight(f"{'lastKnownParent':<20}: {entries['lastKnownParent']}")
+            context.log.highlight(f"{'sAMAccountName':<20}: {entries.get('sAMAccountName', '')}")
+            context.log.highlight(f"{'dn':<20}: {entries.get('distinguishedName', '')}")
+            context.log.highlight(f"{'ID':<20}: {entries.get('name', '').split(':')[1]}")
+            context.log.highlight(f"{'isDeleted':<20}: {entries.get('isDeleted', '')}")
+            context.log.highlight(f"{'lastKnownParent':<20}: {entries.get('lastKnownParent', '')}")
             context.log.highlight("")
+
+            number_of_deleted_objects += 1
+
+        context.log.highlight(f"Found {number_of_deleted_objects} deleted objects")
