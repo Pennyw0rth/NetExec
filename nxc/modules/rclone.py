c6972c14342ea7aa01aa485d05a20fd06242f013
Alexander Neff <alex99.neff@gmx.de>
2026-07-26 06:21:42 -0400

    Move deobfuscation functions into the module

--- nxc/modules/rclone.py
+++ nxc/modules/rclone.py
@@ -3,22 +3,6 @@ from Crypto.Cipher import AES
 from io import BytesIO
 from nxc.helpers.misc import CATEGORY
 
-SECRET_KEY = b"\x9c\x93\x5b\x48\x73\x0a\x55\x4d\x6b\xfd\x7c\x63\xc8\x86\xa9\x2b\xd3\x90\x19\x8e\xb8\x12\x8a\xfb\xf4\xde\x16\x2b\x8b\x95\xf6\x38"
-
-
-def base64_urlsafedecode(string):
-    padding = 4 - (len(string) % 4)
-    string += "=" * padding
-    return base64.urlsafe_b64decode(string)
-
-
-def deobscure(obscured):
-    encrypted_password = base64_urlsafedecode(obscured)
-    iv = encrypted_password[:AES.block_size]
-    buf = encrypted_password[AES.block_size:]
-    crypter = AES.new(key=SECRET_KEY, mode=AES.MODE_CTR, initial_value=iv, nonce=b"")
-    return crypter.decrypt(buf).decode("utf-8")
-
 
 class NXCModule:
     name = "rclone"
@@ -37,7 +21,7 @@ class NXCModule:
         try:
             entries = connection.conn.listPath(self.share, base_dir + "\\*")
         except Exception as e:
-            context.log.error(f"Failed to list {base_dir}: {e}")
+            context.log.fail(f"Failed to list {base_dir}: {e}")
             return False
 
         usernames = [
@@ -81,7 +65,7 @@ class NXCModule:
                 key, val = map(str.strip, line.split("=", 1))
                 if key.lower() in ("pass", "password", "password2"):
                     try:
-                        plain = deobscure(val)
+                        plain = self.deobscure(val)
                         context.log.highlight(f"{key} = {plain}")
                     except Exception as e:
                         context.log.warning(f"[{username}] Failed to deobscure {key}: {e}")
@@ -89,3 +73,16 @@ class NXCModule:
                     context.log.highlight(line.strip())
 
         return True
+
+    def deobscure(self, obscured):
+        encrypted_password = self.base64_urlsafedecode(obscured)
+        iv = encrypted_password[:AES.block_size]
+        buf = encrypted_password[AES.block_size:]
+        SECRET_KEY = b"\x9c\x93\x5b\x48\x73\x0a\x55\x4d\x6b\xfd\x7c\x63\xc8\x86\xa9\x2b\xd3\x90\x19\x8e\xb8\x12\x8a\xfb\xf4\xde\x16\x2b\x8b\x95\xf6\x38"
+        crypter = AES.new(key=SECRET_KEY, mode=AES.MODE_CTR, initial_value=iv, nonce=b"")
+        return crypter.decrypt(buf).decode("utf-8")
+
+    def base64_urlsafedecode(self, string):
+        padding = 4 - (len(string) % 4)
+        string += "=" * padding
+        return base64.urlsafe_b64decode(string)
