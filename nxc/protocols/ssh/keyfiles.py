ed9fb8b33f396c46a84a09b7ec404369c93b8f52
Alexander Neff <alex99.neff@gmx.de>
2026-09-23 14:10:58 -0400

    Formatting

--- nxc/protocols/ssh/keyfiles.py
+++ nxc/protocols/ssh/keyfiles.py
@@ -1,4 +1,3 @@
-
 # Substrings paramiko raises when the server refuses a public key whose algorithm is not in its accepted list, e.g. sshd set to "PubkeyAcceptedAlgorithms ssh-ed25519" while the key is RSA.
 # They must be told apart from a wrong passphrase, otherwise the operator gets an error pointing at the wrong thing.
 KEY_TYPE_REJECTED_SUBSTRINGS = (
